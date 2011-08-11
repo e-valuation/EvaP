589f4c7b1b621bb28b289603f70688d5e3d0a6e2
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-08-11 13:00:13 +0200

    verbose names for the models (for admin etc.)

--- evap/evaluation/models.py
+++ evap/evaluation/models.py
@@ -1,50 +1,72 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class Semester(models.Model):
-    name_de = models.CharField(max_length=100)
-    name_en = models.CharField(max_length=100)
+    name_de = models.CharField(max_length=100, verbose_name=_(u"name (german)"))
+    name_en = models.CharField(max_length=100, verbose_name=_(u"name (english)"))
+    
+    class Meta:
+        verbose_name = _(u"semester")
+        verbose_name_plural = _(u"semesters")
 
 
 class Course(models.Model):
     """Models a single course, i.e. the Math 101 course of 2002."""
 
-    semester = models.ForeignKey(Semester)
-    name_de = models.CharField(max_length=100)
-    name_en = models.CharField(max_length=100)
-    participants = models.ManyToManyField(User)
+    semester = models.ForeignKey(Semester, verbose_name=_(u"semester"))
+    name_de = models.CharField(max_length=100, verbose_name=_(u"name (german)"))
+    name_en = models.CharField(max_length=100, verbose_name=_(u"name (english)"))
+    participants = models.ManyToManyField(User, verbose_name=_(u"participants"))
     
-    vote_start_date = models.DateField(null=True)
-    vote_end_date = models.DateField(null=True)
+    vote_start_date = models.DateField(null=True, verbose_name=_(u"first date to vote"))
+    vote_end_date = models.DateField(null=True, verbose_name=_(u"last date to vote"))
     
-    publish_date = models.DateField(null=True)
+    publish_date = models.DateField(null=True, verbose_name=_(u"publishing date"))
+    
+    class Meta:
+        verbose_name = _(u"course")
+        verbose_name_plural = _(u"courses")
 
 
 class QuestionGroup(models.Model):
     """A named collection of questions."""
     
-    name_de = models.CharField(max_length=100)
-    name_en = models.CharField(max_length=100)
+    name_de = models.CharField(max_length=100, verbose_name=_(u"name (german)"))
+    name_en = models.CharField(max_length=100, verbose_name=_(u"name (english)"))
+    
+    class Meta:
+        verbose_name = _(u"question group")
+        verbose_name_plural = _(u"question groups")
 
 
 class Question(models.Model):
     """A question including a type."""
     
     QUESTION_KINDS = (
-        (u"T", u"Text Question"),
-        (u"G", u"Grade Question")
+        (u"T", _(u"Text Question")),
+        (u"G", _(u"Grade Question"))
     )
     
     question_group = models.ForeignKey(QuestionGroup)
-    text_de = models.TextField()
-    text_en = models.TextField()
-    kind = models.CharField(max_length=1, choices=QUESTION_KINDS)
+    text_de = models.TextField(verbose_name=_(u"question text (german)"))
+    text_en = models.TextField(verbose_name=_(u"question text (english)"))
+    kind = models.CharField(max_length=1, choices=QUESTION_KINDS,
+                            verbose_name=_(u"kind of question"))
+    
+    class Meta:
+        verbose_name = _(u"question")
+        verbose_name_plural = _(u"questions")
 
 
 class Questionnaire(models.Model):
     """A questionnaire connects a course and optionally a lecturer to a question group."""
-    course = models.ForeignKey(Course)
-    question_group = models.ForeignKey(QuestionGroup)
+    course = models.ForeignKey(Course, verbose_name=_(u"course"))
+    question_group = models.ForeignKey(QuestionGroup, verbose_name=_(u"question group"))
+    
+    class Meta:
+        verbose_name = _(u"questionnaire")
+        verbose_name_plural = _(u"questionnaires")
 
 
 class Answer(models.Model):
@@ -55,13 +77,16 @@ class Answer(models.Model):
     questionnaire = models.ForeignKey(Questionnaire)
     
     class Meta:
+        verbose_name = _(u"answer")
+        verbose_name_plural = _(u"answers")
+        
         abstract = True
 
 
 class GradeAnswer(Answer):
-    answer = models.IntegerField()
+    answer = models.IntegerField(verbose_name = _(u"answer"))
 
 
 class TextAnswer(Answer):
-    answer = models.TextField()
+    answer = models.TextField(verbose_name = _(u"answer"))
 
