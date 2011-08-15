f9506e012e525cf29b13c3d5d165332662ca494b
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-08-16 01:25:24 +0200

    whoops, databases with two names break django's test infrastructure

--- evap/settings.py
+++ evap/settings.py
@@ -14,7 +14,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'development': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3', # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(SITE_ROOT, 'database.sqlite3'), # Or path to database file if using sqlite3.
         'USER': '',                             # Not used with sqlite3.
@@ -23,8 +23,6 @@ DATABASES = {
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
-
-DATABASES['default'] = DATABASES['development']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
