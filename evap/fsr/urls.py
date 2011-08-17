87e4b1cceadd0f6ec5fe12e5b22c2d365eeaf5f2
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-08-18 01:43:30 +0200

    write that shorter

--- evap/fsr/urls.py
+++ evap/fsr/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r"^$", 'fsr.views.fsr_index'),
-    url(r"^create$", 'fsr.views.fsr_semester_create'),
-    url(r"^(\d+)$", 'fsr.views.fsr_semester_view'),
-    url(r"^(\d+)/create$", 'fsr.views.fsr_course_create'),
-    url(r"^(\d+)/edit$", 'fsr.views.fsr_semester_edit'),
-    url(r"^(\d+)/import$", 'fsr.views.fsr_semester_import'),
-    url(r"^(\d+)/(\d+)/edit$", 'fsr.views.fsr_course_edit'),
+urlpatterns = patterns('fsr.views',
+    url(r"^$", 'fsr_index'),
+    url(r"^create$", 'fsr_semester_create'),
+    url(r"^(\d+)$", 'fsr_semester_view'),
+    url(r"^(\d+)/create$", 'fsr_course_create'),
+    url(r"^(\d+)/edit$", 'fsr_semester_edit'),
+    url(r"^(\d+)/import$", 'fsr_semester_import'),
+    url(r"^(\d+)/(\d+)/edit$", 'fsr_course_edit'),
 )
