d8f5d2704510292d5340ec6bd6b1415fbe860ca1
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-08-17 16:40:17 +0200

    set languages manually

--- evap/settings.py
+++ evap/settings.py
@@ -37,6 +37,11 @@ TIME_ZONE = 'Europe/Berlin'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
 
+LANGUAGES = (
+    ('en', "English"),
+    ('de', "Deutsch"),
+)
+
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
