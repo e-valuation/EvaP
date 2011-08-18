cb066028305c7a95892921c2af80d6827b4df471
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-08-18 02:50:25 +0200

    don't like those long view names :)

--- evap/fsr/urls.py
+++ evap/fsr/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('fsr.views',
-    url(r"^$", 'fsr_index'),
-    url(r"^create$", 'fsr_semester_create'),
-    url(r"^(\d+)$", 'fsr_semester_view'),
-    url(r"^(\d+)/create$", 'fsr_course_create'),
-    url(r"^(\d+)/edit$", 'fsr_semester_edit'),
-    url(r"^(\d+)/import$", 'fsr_semester_import'),
-    url(r"^(\d+)/(\d+)/edit$", 'fsr_course_edit'),
+    url(r"^$", 'index'),
+    url(r"^create$", 'semester_create'),
+    url(r"^(\d+)$", 'semester_view'),
+    url(r"^(\d+)/create$", 'course_create'),
+    url(r"^(\d+)/edit$", 'semester_edit'),
+    url(r"^(\d+)/import$", 'semester_import'),
+    url(r"^(\d+)/(\d+)/edit$", 'course_edit'),
 )
--- evap/fsr/views.py
+++ evap/fsr/views.py
@@ -13,30 +13,30 @@ from fsr.forms import ImportForm, SemesterForm, CourseForm
 from fsr.tools import find_course, find_student
 
 @login_required
-def fsr_index(request):
+def index(request):
     semesters = Semester.objects.all()
     return render_to_response("fsr_index.html", dict(semesters=semesters), context_instance=RequestContext(request))
 
 @login_required
-def fsr_semester_view(request, semester_id):
+def semester_view(request, semester_id):
     semester = get_object_or_404(Semester, id=semester_id)
     courses = semester.course_set.all()
     return render_to_response("fsr_semester_view.html", dict(semester=semester, courses=courses), context_instance=RequestContext(request))
 
 @login_required
-def fsr_semester_create(request):
+def semester_create(request):
     form = SemesterForm(request.POST or None)
     
     if form.is_valid():
         s = form.save()
         
         messages.add_message(request, messages.INFO, _("Successfully created semester."))
-        return redirect('fsr.views.fsr_semester_view', s.id)
+        return redirect('fsr.views.semester_view', s.id)
     else:
         return render_to_response("fsr_semester_create.html", dict(form=form), context_instance=RequestContext(request))
 
 @login_required
-def fsr_semester_edit(request, semester_id):
+def semester_edit(request, semester_id):
     semester = get_object_or_404(Semester, id=semester_id)
     form = SemesterForm(request.POST or None, instance = semester)
     
@@ -44,12 +44,12 @@ def fsr_semester_edit(request, semester_id):
         form.save()
         
         messages.add_message(request, messages.INFO, _("Successfully updated semester."))
-        return redirect('fsr.views.fsr_semester_view', semester_id)
+        return redirect('fsr.views.semester_view', semester_id)
     else:
         return render_to_response("fsr_semester_edit.html", dict(semester=semester, form=form), context_instance=RequestContext(request))
 
 @login_required
-def fsr_semester_import(request, semester_id):   
+def semester_import(request, semester_id):   
     semester = get_object_or_404(Semester, id=semester_id)
     form = ImportForm(request.POST or None, request.FILES or None)
     
@@ -74,12 +74,12 @@ def fsr_semester_import(request, semester_id):
                     raise
         
         messages.add_message(request, messages.INFO, _("Import successful."))
-        return redirect('fsr.views.fsr_semester_view', semester_id)
+        return redirect('fsr.views.semester_view', semester_id)
     else:
         return render_to_response("fsr_import.html", dict(semester=semester, form=form), context_instance=RequestContext(request))
 
 @login_required
-def fsr_course_create(request, semester_id):
+def course_create(request, semester_id):
     semester = get_object_or_404(Semester, id=semester_id)
     form = CourseForm(request.POST or None)
     
@@ -87,12 +87,12 @@ def fsr_course_create(request, semester_id):
         form.save()
         
         messages.add_message(request, messages.INFO, _("Successfully created course."))
-        return redirect('fsr.views.fsr_semester_view', semester_id)
+        return redirect('fsr.views.semester_view', semester_id)
     else:
         return render_to_response("fsr_course_create.html", dict(semester=semester, form=form), context_instance=RequestContext(request))
 
 @login_required
-def fsr_course_edit(request, semester_id, course_id):
+def course_edit(request, semester_id, course_id):
     semester = get_object_or_404(Semester, id=semester_id)
     course = get_object_or_404(Course, id=course_id)
     form = CourseForm(request.POST or None, instance = course)
@@ -101,7 +101,7 @@ def fsr_course_edit(request, semester_id, course_id):
         form.save()
         
         messages.add_message(request, messages.INFO, _("Successfully updated course."))
-        return redirect('fsr.views.fsr_semester_view', semester_id)
+        return redirect('fsr.views.semester_view', semester_id)
     else:
         return render_to_response("fsr_course_edit.html", dict(semester=semester, form=form), context_instance=RequestContext(request))
     
