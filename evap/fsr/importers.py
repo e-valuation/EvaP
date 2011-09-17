d5cd0da6b8c9bd6d99c5702f25448aeab417b528
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-09-17 14:48:34 +0200

    refactoring the importer, fixes #22

--- evap/fsr/importers.py
+++ evap/fsr/importers.py
@@ -6,14 +6,47 @@ from django.utils.translation import ugettext as _
 
 from evaluation.models import Course, Semester
 
-import collections
 import xlrd
 
-import traceback
 
-UserData = collections.namedtuple('UserData', ('username', 'first_name', 'last_name'))
-UserProfileData = collections.namedtuple('UserProfileData', ('title',))
-CourseData = collections.namedtuple('CourseData', ('name_de', 'name_en', 'kind'))
+class UserData(object):
+    """Holds information about a user, retrieved from the Excel file."""
+    
+    def __init__(self, username=None, first_name=None, last_name=None, title=None):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.title = title
+    
+    def store_in_database(self):
+        user = User(username=self.username,
+                    first_name=self.first_name,
+                    last_name=self.last_name)
+        user.save()
+        profile = user.get_profile()
+        profile.title = self.title
+        profile.save()
+        return user
+
+
+class CourseData(object):
+    """Holds information about a course, retrieved from the Excel file."""
+    
+    def __init__(self, name_de=None, name_en=None, kind=None):
+        self.name_de = name_de
+        self.name_en = name_en
+        self.kind = kind
+    
+    def store_in_database(self, vote_start_date, vote_end_date, semester):
+        course = Course(name_de=self.name_de,
+                        name_en=self.name_en,
+                        kind=self.kind,
+                        vote_start_date=vote_start_date,
+                        vote_end_date=vote_end_date,
+                        semester=semester)
+        course.save()
+        return course
+
 
 class ExcelImporter(object):
     def __init__(self, request):
@@ -22,7 +55,7 @@ class ExcelImporter(object):
     
     def read_file(self, excel_file):
         """Reads an excel file and stores all the student-lecturer-course
-        associations in the associations member."""
+        associations in the `associations` member."""
         
         book = xlrd.open_workbook(file_contents=excel_file.read())
         
@@ -33,26 +66,28 @@ class ExcelImporter(object):
                     data = sheet.row_values(row)
                     # assign data to data objects
                     student_data = UserData(username=data[3], first_name=data[2], last_name=data[1])
-                    
-                    lecturer_data = UserData(username=data[9], first_name="", last_name=data[8])
-                    lecturer_profile_data = UserProfileData(title=data[7])
-                    
+                    lecturer_data = UserData(username=data[9], first_name="", last_name=data[8], title=data[7])
                     course_data = CourseData(name_de=data[5], name_en=data[6], kind=data[4])
                     
                     # store data objects together with the data source location for problem tracking
-                    self.associations[(sheet.name, row)] = (student_data, lecturer_data, lecturer_profile_data, course_data)
+                    self.associations[(sheet.name, row)] = (student_data, lecturer_data, course_data)
                 messages.info(self.request, _(u"Successfully read sheet '%s'.") % sheet.name)
             except:
                 messages.warning(self.request, _(u"A problem occured while reading sheet '%s'.") % sheet.name)
                 raise
         messages.info(self.request, _(u"Successfully read excel file."))
     
-    def validate(self):
-        """Validates the internal integrity of the data read by read_file. Does
-        not validate against data already in the database."""
+    def validate_and_fix(self):
+        """Validates the internal integrity of the data read by read_file and
+        fixes and inferres data if possible. Should not validate against data
+        already in the database."""
         
-        # FIXME: implement *relevant* validations
-        pass
+        for (sheet, row), (student_data, lecturer_data, course_data) in self.associations.items():
+            # try to infer first names from usernames
+            if not lecturer_data.first_name:
+                first, sep, last = lecturer_data.username.partition(".")
+                if sep == ".":
+                    lecturer_data.first_name = first
     
     def save_to_db(self, semester, vote_start_date, vote_end_date):
         """Stores the read and validated data in the database. Errors might still
@@ -63,28 +98,31 @@ class ExcelImporter(object):
             course_count = 0
             student_count = 0
             lecturer_count = 0
-            for (sheet, row), (student_data, lecturer_data, lecturer_profile_data, course_data) in self.associations.items():
+            for (sheet, row), (student_data, lecturer_data, course_data) in self.associations.items():
                 try:
                     # create or retrieve database objects
-                    student, student_is_new = User.objects.get_or_create(username=student_data.username, defaults=student_data._asdict())
+                    try:
+                        student = User.objects.get(username=student_data.username)
+                    except User.DoesNotExist:
+                        student = student_data.store_in_database()
+                        student_count += 1
                     
-                    lecturer, lecturer_is_new = User.objects.get_or_create(username=lecturer_data.username, defaults=lecturer_data._asdict())
-                    lecturer.get_profile().title = lecturer_profile_data.title
-                    lecturer.get_profile().save()
+                    try:
+                        lecturer = User.objects.get(username=lecturer_data.username)
+                    except User.DoesNotExist:
+                        lecturer = lecturer_data.store_in_database()
+                        lecturer_count += 1
                     
-                    course, course_is_new = Course.objects.get_or_create(semester=semester, name_de=course_data.name_de, defaults=dict(course_data._asdict().items() + [('vote_start_date', vote_start_date), ('vote_end_date',vote_end_date)]))
-                    
+                    try:
+                        course = Course.objects.get(semester=semester, name_de=course_data.name_de)
+                    except Course.DoesNotExist:
+                        course = course.store_in_database(vote_start_date, vote_end_date, semester)
+                        course_count += 1
+                                        
                     # connect database objects
                     course.participants.add(student)
                     course.primary_lecturers.add(lecturer)
                     
-                    # remember new items
-                    if course_is_new:
-                        course_count += 1
-                    if student_is_new:
-                        student_count += 1
-                    if lecturer_is_new:
-                        lecturer_count += 1
                 except Exception, e:
                     messages.warning(self.request, _("A problem occured while writing the entries to the database. " \
                                                      "The original data location was row %(row)d of sheet '%(sheet)s'. " \
@@ -99,8 +137,7 @@ class ExcelImporter(object):
         try:
             importer = cls(request)
             importer.read_file(excel_file)
-            importer.validate()
+            importer.validate_and_fix()
             importer.save_to_db(semester, vote_start_date, vote_end_date)
         except Exception, e:
             messages.error(request, _(u"Import finally aborted after exception: '%s'" % e))
-            traceback.print_exc()
