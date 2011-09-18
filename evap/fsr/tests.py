208ac95c321ec6fe12e6ca340ae11f6a5e1b4ba1
Michael Gruenewald <mail@michaelgruenewald.eu>
2011-09-18 15:26:30 +0200

    a first usecase test

--- evap/fsr/tests.py
+++ evap/fsr/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
+from django_webtest import WebTest
 
+from django.contrib.auth.models import User
 from evaluation.models import Semester, Questionnaire
+
+import os.path
 
 class SimpleViewTestsTest(TestCase):
     fixtures = ['simple-tests']
@@ -31,3 +35,37 @@ class SimpleViewTestsTest(TestCase):
         self.client.get('/fsr/questionnaire/%d/edit' % questionnaire.id)
         self.client.get('/fsr/questionnaire/%d/copy' % questionnaire.id)
         self.client.get('/fsr/questionnaire/%d/delete' % questionnaire.id)
+
+
+class UsecaseTests(WebTest):
+    fixtures = ['usecase-tests']
+    
+    extra_environ = {'HTTP_ACCEPT_LANGUAGE': 'en'}
+    
+    def test_import(self):
+        # login first
+        index = self.app.get("/", user="fsr.user")
+        
+        # create a new semester
+        p = self.app.get("/fsr/semester/create")
+        semester_form = p.forms[0]
+        semester_form['name_de'] = "Testsemester"
+        semester_form['name_en'] = "test semester"
+        p = semester_form.submit().follow()
+        
+        # retrieve new semester
+        semester = Semester.objects.get(name_de="Testsemester",
+                                        name_en="test semester")
+        
+        assert semester.course_set.count() == 0
+        
+        p = p.click("[Ii]mport")
+        upload_form = p.forms[0]
+        upload_form['vote_start_date'] = "02/29/2000"
+        upload_form['vote_end_date'] = "02/29/2012"
+        upload_form['excel_file'] = (os.path.join(os.path.dirname(__file__), "fixtures", "samples.xls"),)
+        p = upload_form.submit().follow()
+        
+        assert semester.course_set.count() == 23
+        assert User.objects.count() == 25
+    
