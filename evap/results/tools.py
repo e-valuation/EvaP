04f9f2b9a31559e60555397cb05f6f4c701ab080
Stefan Richter <stefanc.richter@student.hpi.uni-potsdam.de>
2012-01-04 22:07:31 +0100

    fix menu bug -  listing too many semesters

--- evap/results/tools.py
+++ evap/results/tools.py
@@ -2,5 +2,5 @@ from evap.evaluation.models import Semester
 
 
 def all_semesters():
-    semesters = Semester.objects.all()
+    semesters = Semester.objects.filter(course__state="published")
     return {'semesters': semesters}
