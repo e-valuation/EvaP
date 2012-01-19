d32e95e7c4bd178bb0d5eca9d97db33da170b732
Michael Gruenewald <mail@michaelgruenewald.eu>
2012-01-19 11:00:03 +0100

    better update script

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
 pushd `dirname $0`/evap
-python manage.py migrate && python manage.py collectstatic --noinput && python manage.py compilemessages && /etc/init.d/apache2 restart
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py compilemessages
+/etc/init.d/apache2 restart
+echo Update completed.
 popd
