e-valuation/EvaP

commit c9953d259af573c732e3fb520bc74db2587aa42f

solving some problems with untranslated form fields (e.g. 'Proxies')

evap/fsr/forms.py
@@ -285,30 +285,27 @@ class SelectCourseForm(forms.Form, BootstrapMixin):
 
     
 class UserForm(forms.ModelForm, BootstrapMixin):
-    username = forms.CharField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.CharField(required=False)
-    fsr = forms.BooleanField(required=False, label=_("FSR Member"))
-    proxies = UserModelMultipleChoiceField(queryset=User.objects.order_by("username"))
+    # steal form field definitions for the User model
+    locals().update(forms.fields_for_model(User, fields=('username', 'first_name', 'last_name', 'email', 'is_staff')))
     
     class Meta:
         model = UserProfile
-        exclude = ('user',)
-        fields = ['username', 'title', 'first_name', 'last_name', 'email', 'picture', 'proxies', 'is_lecturer']
+        fields = ('username', 'title', 'first_name', 'last_name', 'email', 'picture', 'proxies', 'is_staff', 'is_lecturer')
     
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
         
         # fix generated form
         self.fields['proxies'].required = False
+        self.fields['proxies'].queryset = User.objects.order_by("username")
+        self.fields['is_staff'].label = label=_(u"FSR Member")
         
         # load user fields
         self.fields['username'].initial = self.instance.user.username
         self.fields['first_name'].initial = self.instance.user.first_name
         self.fields['last_name'].initial = self.instance.user.last_name
         self.fields['email'].initial = self.instance.user.email
-        self.fields['fsr'].initial = self.instance.user.is_staff
+        self.fields['is_staff'].initial = self.instance.user.is_staff
 
     def save(self, *args, **kw):
         # first save the user, so that the profile gets created for sure

evap/lecturer/forms.py
@@ -46,11 +46,8 @@ class CourseForm(forms.ModelForm, BootstrapMixin):
 
 
 class UserForm(forms.ModelForm, BootstrapMixin):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.CharField(required=False)
-    
-    proxies = UserModelMultipleChoiceField(queryset=User.objects.order_by("username"))
+    # steal form field definitions for the User model
+    locals().update(forms.fields_for_model(User, fields=('first_name', 'last_name', 'email')))
     
     class Meta:
         model = UserProfile
@@ -61,6 +58,7 @@ class UserForm(forms.ModelForm, BootstrapMixin):
         
         # fix generated form
         self.fields['proxies'].required = False
+        self.fields['proxies'].queryset = User.objects.order_by("username")
         
         # load user fields
         self.fields['first_name'].initial = self.instance.user.first_name

commit 3c92a7d885816eddb3f722ef8fe30da3131982cc

fix for bug introduced with c9953d259af573c732e3fb520bc74db2587aa42f

evap/fsr/forms.py
@@ -313,7 +313,7 @@ class UserForm(forms.ModelForm, BootstrapMixin):
         self.instance.user.first_name = self.cleaned_data.get('first_name')
         self.instance.user.last_name = self.cleaned_data.get('last_name')
         self.instance.user.email = self.cleaned_data.get('email')
-        self.instance.user.is_staff = self.cleaned_data.get('fsr')
+        self.instance.user.is_staff = self.cleaned_data.get('is_staff')
         self.instance.user.save()
         self.instance = self.instance.user.get_profile()
         