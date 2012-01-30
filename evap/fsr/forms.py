901caadf6bf43a7be8bf092ad62e97a0c534f0ab
Stefan Richter <stefan@02strich.de>
2012-01-30 16:06:17 +0100

    making dates non localised - seems to collide with JS

--- evap/fsr/forms.py
+++ evap/fsr/forms.py
@@ -49,6 +49,8 @@ class CourseForm(forms.ModelForm, BootstrapMixin):
     def __init__(self, *args, **kwargs):
         super(CourseForm, self).__init__(*args, **kwargs)
         
+        self.fields['vote_start_date'].localize = False
+        self.fields['vote_end_date'].localize = False
         self.fields['kind'].widget = forms.Select(choices=[(a, a) for a in Course.objects.values_list('kind', flat=True).order_by().distinct()])
         self.fields['study'].widget = forms.Select(choices=[(a, a) for a in Course.objects.values_list('study', flat=True).order_by().distinct()])
         self.fields['participants'].queryset=User.objects.order_by("last_name", "first_name", "username")
