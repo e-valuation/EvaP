eb41bc40be4bf6d0a1351081e192dac65895129c
Stefan Richter <stefan@02strich.de>
2012-02-09 14:44:36 +0100

    adding user merging

---
+++ evap/evaluation/management/commands/merge_users.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+
+from evap.evaluation.models import User
+from evap.evaluation.merge import merge_model_objects
+
+class Command(BaseCommand):
+    args = '<user ID 1> <user ID 2>'
+    help = 'Merge two users'
+    
+    def handle(self, *args, **options):
+        try:
+            user1 = User.objects.get(pk=int(args[0]))
+            user2 = User.objects.get(pk=int(args[1]))
+            
+            print "Merging user '{1}' into user '{0}'".format(user1, user2)            
+            merge_model_objects(user1, user2)            
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
