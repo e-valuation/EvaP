8e514b808ebba2f2cabdc956e162195e1f23abbe
Stefan Richter <stefan@02strich.de>
2012-02-11 20:09:57 +0100

    allow lecturer preview allways and provide read-only way to allways view course-data - closes #64

--- evap/lecturer/urls.py
+++ evap/lecturer/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('evap.lecturer.views',
     url(r"^$", 'index'),
     url(r"^profile$", 'profile_edit'),
-    url(r"^course/(\d+)$", 'course_edit'),
+    url(r"^course/(\d+)$", 'course_view'),
+    url(r"^course/(\d+)/edit$", 'course_edit'),
     url(r"^course/(\d+)/preview$", 'course_preview'),
 )
