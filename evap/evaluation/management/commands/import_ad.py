afd49cb93aca64f17be04a21fd3f3acbf74257ac
Stefan Richter <stefan@02strich.de>
2012-02-11 19:40:24 +0100

    adding first name, last name, email and lecturer bit importing from ad - closes #25

---
+++ evap/evaluation/management/commands/import_ad.py
@@ -0,0 +1,50 @@
+import getpass
+import ldap
+import sys
+
+from django.core import exceptions
+from django.core.management.base import BaseCommand
+from django.contrib.auth.management.commands.createsuperuser import RE_VALID_USERNAME, EMAIL_RE
+from django.utils.translation import ugettext as _
+
+from evap.evaluation.models import User
+
+
+class Command(BaseCommand):
+    args = '<ldap server> <username>'
+    help = 'Imports user data from Active Directory. The username should be specified with realm.'
+    
+    def handle(self, *args, **options):
+        try:
+            # connect
+            l = ldap.initialize(args[0])
+            
+            # bind
+            l.bind_s(args[1], getpass.getpass("AD Password: "))
+            
+            # find all users
+            result = l.search_s("OU=INSTITUT,DC=hpi,DC=uni-potsdam,DC=de", ldap.SCOPE_SUBTREE, filterstr="(&(&(objectClass=user)(!(objectClass=computer)))(givenName=*)(sn=*)(mail=*))")
+            for dn, attrs in result:
+                try:
+                    user = User.objects.get(username__iexact=attrs['sAMAccountName'][0])
+                    user.first_name = attrs['givenName'][0]
+                    user.last_name = attrs['sn'][0]
+                    user.email = attrs['mail'][0]
+                    user.save()
+                    
+                    p = user.get_profile()
+                    if not p.is_lecturer:
+                        p.is_lecturer = dn.endswith("OU=Mitarbeiter,OU=INSTITUT,DC=hpi,DC=uni-potsdam,DC=de")
+                    p.save()
+                    
+                    print "Successfully updated: '{0}'".format(user.username)
+                except User.DoesNotExist:
+                    pass
+                except Exception as e:
+                    print e
+            
+            l.unbind_s()
+        
+        except KeyboardInterrupt:
+            sys.stderr.write("\nOperation cancelled.\n")
+            sys.exit(1)
