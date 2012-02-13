3c1ae0a9a1ac47bf5971ed093c1a566c8acf2162
Stefan Richter <stefan@02strich.de>
2012-02-13 16:48:05 +0100

    now using rebase on server

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 pushd `dirname $0`
-ssh-agent bash -c "ssh-add deployment_key; git pull"
+ssh-agent bash -c "ssh-add deployment_key; git fetch"
+git rebase origin
 pushd `dirname $0`/evap
 python manage.py migrate
 python manage.py collectstatic --noinput
