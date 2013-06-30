b3123d35c7c5c6df0e2b02fb4286407c78bd2ed7
Stefan Richter <stefan@02strich.de>
2013-06-30 12:43:42 +0200

    Moving to django 1.5 step 1

--- evap/settings.py
+++ evap/settings.py
@@ -197,9 +197,15 @@ if not DEBUG:
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+     },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'console': {
