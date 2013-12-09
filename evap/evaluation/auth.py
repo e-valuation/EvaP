5fad4950432c71ae14fe80b38c988e5860886f5e
Stefan Richter <stefan@02strich.de>
2013-12-09 23:20:07 +0100

    get rid of old authentication hacks

--- evap/evaluation/auth.py
+++ evap/evaluation/auth.py
@@ -53,64 +53,6 @@ class RequestAuthMiddleware(object):
             messages.warning(request, _(u"Invalid login key."))
 
 
-class CaseInsensitiveModelBackend(ModelBackend):
-    """
-    By default ModelBackend does case _sensitive_ username authentication, which isn't what is
-    generally expected.  This backend supports case insensitive username authentication.
-    """
-    def authenticate(self, username=None, password=None, **kwargs):
-        UserModel = auth.get_user_model()
-        if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
-        if username is None:
-            return None
-        try:
-            user = UserModel._default_manager.get(**{UserModel.USERNAME_FIELD + "__iexact":username})
-            if user.check_password(password):
-                return user
-        except UserModel.DoesNotExist:
-            return None
-
-
-class CaseInsensitiveRemoteUserBackend(RemoteUserBackend):
-    """
-    By default RemoteUserBackend does case _sensitive_ username authentication, which isn't what is
-    generally expected.  This backend supports case insensitive username authentication.
-    """
-    def authenticate(self, remote_user):
-        """
-        The username passed as ``remote_user`` is considered trusted.  This
-        method simply returns the ``User`` object with the given username,
-        creating a new ``User`` object if ``create_unknown_user`` is ``True``.
-        
-        Returns None if ``create_unknown_user`` is ``False`` and a ``User``
-        object with the given username is not found in the database.
-        """
-        
-        if not remote_user:
-            return
-        
-        user = None
-        username = self.clean_username(remote_user)
-        
-        # Note that this could be accomplished in one try-except clause, but
-        # instead we use get_or_create when creating unknown users since it has
-        # built-in safeguards for multiple threads.
-        if self.create_unknown_user:
-            user, created = User.objects.get_or_create(username__iexact=username, defaults={'username': username, 'email': 'user@example.com'})
-            if created:
-                user = self.configure_user(user)
-        else:
-            try:
-                user = User.objects.get(username__iexact=username)
-            except User.DoesNotExist:
-                pass
-        return user
-    
-    def clean_username(self, username):
-        return username.partition("@")[0]
-
-
 class RequestAuthUserBackend(ModelBackend):
     """
     The RequestAuthBackend works together with the RequestAuthMiddleware to
