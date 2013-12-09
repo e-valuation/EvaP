7d17ffedea5bdf2b0cc5bd88a1ce7be2cc60fb44
Stefan Richter <stefan@02strich.de>
2013-12-09 23:21:05 +0100

    integrate all login possibilities on one page
    known: copy is bad

--- evap/evaluation/views.py
+++ evap/evaluation/views.py
@@ -1,40 +1,66 @@
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
 
-from evap.evaluation.forms import NewKeyForm
+from evap.evaluation.forms import NewKeyForm, LoginKeyForm, LoginUsernameForm
 from evap.evaluation.models import UserProfile
 from evap.fsr.models import EmailTemplate
 
 
 def index(request):
-    new_key_form = NewKeyForm(request.POST or None)
-    
+    """Main entry page into EvaP providing all the login options available. THe username/password 
+       login is thought to be used for internal users, e.g. by connecting to a LDAP directory.
+       The login key mechanism is meant to be used to include external participants, e.g. visiting 
+       students or visiting lecturers.
+    """
+
+    # parse the form data into the respective form
+    submit_type = request.POST.get("submit_type", "no_submit")
+    new_key_form = NewKeyForm(request.POST if submit_type == "new_key" else None)
+    login_key_form = LoginKeyForm(request.POST if submit_type == "login_key" else None)
+    login_username_form = LoginUsernameForm(request, request.POST if submit_type == "login_username" else None)
+
+    # process form data
     if request.method == 'POST':
         if new_key_form.is_valid():
             # user wants a new login key
-            try:
-                user = User.objects.get(email__iexact=new_key_form.cleaned_data['email'])
-                profile = UserProfile.get_for_user(user)
-                profile.generate_logon_key()
-                profile.save()
-                
-                EmailTemplate.get_logon_key_template().send_user(user)
-                
-            except User.DoesNotExist:
-                messages.warning(request, _(u"No user with this e-mail address was found."))
+
+            # check if we got a domain user
+            if new_key_form.cleaned_data['email'].endswith("hpi.uni-potsdam.de"):
+                messages.warning(request, _(u"HPI users cannot request login keys. Please login using your domain credentials."))
+            else:
+                # non HPI email, send him/her a new logon key
+                try:
+                    user = User.objects.get(email__iexact=new_key_form.cleaned_data['email'])
+                    profile = UserProfile.get_for_user(user)
+                    profile.generate_logon_key()
+                    profile.save()
+                    
+                    EmailTemplate.get_logon_key_template().send_user(user)
+                    
+                    messages.success(request, _(u"Successfully sent email with new login key."))
+                except User.DoesNotExist:
+                    messages.warning(request, _(u"No user with this e-mail address was found."))
+        elif login_key_form.is_valid():
+            # user would like to login with a login key and passed key test
+            auth_login(request, login_key_form.get_user())
+        elif login_username_form.is_valid():
+            # user would like to login with username and password and passed password test
+            auth_login(request, login_username_form.get_user())
+
+            # clean up our test cookie
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
     
+    # if not logged in by now, render form
     if not request.user.is_active:
-        return render_to_response(
-            "index.html",
-            dict(
-                 new_key_form=new_key_form
-            ),
-            context_instance=RequestContext(request))
+        # set test cookie to verify whether they work in the next step
+        request.session.set_test_cookie()
+
+        return render_to_response("index.html", dict(new_key_form=new_key_form, login_key_form=login_key_form, login_username_form=login_username_form), context_instance=RequestContext(request))
     else:
         # redirect user to appropriate start page
         if request.user.is_staff:
@@ -43,33 +69,6 @@ def index(request):
             return redirect('evap.lecturer.views.index')
         else:
             return redirect('evap.student.views.index')
-
-def login(request):
-    # if we already got authentication, e.g. from REMOTE_USER, just go to home
-    if request.user.is_authenticated():
-        return redirect("/")
-    else:
-        # otherwise show login form
-        if request.method == "POST":
-            form = AuthenticationForm(data=request.POST)  
-            
-            # check user input, uncluding authenticating the user
-            if form.is_valid():
-                # we succeeded, so log him in
-                auth_login(request, form.get_user())
-
-                # clean up our test cookie
-                if request.session.test_cookie_worked():
-                    request.session.delete_test_cookie()
-
-                return redirect("/")
-        else:
-            form = AuthenticationForm(request)
-        
-        # set test cookie to verify whether they work in the next step
-        request.session.set_test_cookie()
-            
-        return render_to_response("login.html", dict(form=form), context_instance=RequestContext(request))
 
 
 def faq(request):
