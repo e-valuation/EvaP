eb4dc0d6f0afb0d2162012b989854f5399bb95a5
Stefan Richter <stefan@02strich.de>
2013-12-15 21:39:27 +0100

    update translations

--- evap/settings.py
+++ evap/settings.py
@@ -82,6 +82,11 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+# Locale paths
+LOCALE_PATHS = (
+    os.path.join(SITE_ROOT, "locale"),
+)
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(SITE_ROOT, "upload")
