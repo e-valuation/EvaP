31cac5d51c4a34e01832ee0661243324f64e7787
Stefan Richter <stefan@02strich.de>
2013-12-30 15:01:48 +0100

    remove language menu as its actually not necessary

--- evap/urls.py
+++ evap/urls.py
@@ -15,7 +15,6 @@ urlpatterns = patterns('',
     url(r"^student/", include('evap.student.urls')),
     url(r"^lecturer/", include('evap.lecturer.urls')),
     
-    url(r"^i18n/", include('django.conf.urls.i18n')),
     url(r"^admin/", include(admin.site.urls)),
 )
 
