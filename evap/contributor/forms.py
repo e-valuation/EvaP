b80a30ac508c300634a50643b04919129e3b40e1
Stefan Richter <stefan@02strich.de>
2014-01-05 00:20:34 +0100

    remove stupid help text

--- evap/contributor/forms.py
+++ evap/contributor/forms.py
@@ -58,6 +58,7 @@ class UserForm(forms.ModelForm, BootstrapMixin):
         # fix generated form
         self.fields['delegates'].required = False
         self.fields['delegates'].queryset = User.objects.extra(select={'lower_username': 'lower(username)'}).order_by('lower_username')
+        self.fields['delegates'].help_text = ""
         
         # load user fields
         self.fields['first_name'].initial = self.instance.user.first_name
