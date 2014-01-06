da4d92f0358b87ab4826c8c03283aa191c9fd6aa
Stefan Richter <stefan@02strich.de>
2014-01-06 08:35:46 +0100

    missing bootstrap classes

--- evap/fsr/forms.py
+++ evap/fsr/forms.py
@@ -84,9 +84,14 @@ class CourseForm(forms.ModelForm, BootstrapMixin):
 class ContributionForm(forms.ModelForm, BootstrapMixin):
     class Meta:
         model = Contribution
+        fields = "__all__"
     
     def __init__(self, *args, **kwargs):
         super(ContributionForm, self).__init__(*args, **kwargs)
+        self.fields['contributor'].widget.attrs['class'] = 'form-control'
+        self.fields['responsible'].widget.attrs['class'] = 'form-control'
+        self.fields['can_edit'].widget.attrs['class'] = 'form-control'
+        
         self.fields['contributor'].queryset = User.objects.extra(select={'lower_username': 'lower(username)'}).order_by('lower_username')
         self.fields['questionnaires'] = QuestionnaireMultipleChoiceField(Questionnaire.objects.filter(is_for_contributors=True, obsolete=False), label=_("Questionnaires"))
 
