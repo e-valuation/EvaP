27dee9c276643b0b1a1f69e6e9ca197cc6653f8b
Stefan Richter <stefan@02strich.de>
2014-01-16 12:22:12 +0100

    Add missing space and update the call in another place
    re #230

--- evap/fsr/forms.py
+++ evap/fsr/forms.py
@@ -77,8 +77,8 @@ class CourseForm(forms.ModelForm, BootstrapMixin):
         
         try:
             self.instance.validate_unique(exclude=exclude)
-        except forms.ValidationError, e:
-            self._update_errors(e.message_dict)
+        except forms.ValidationError as e:
+            self._update_errors(e)
 
 
 class ContributionForm(forms.ModelForm, BootstrapMixin):
@@ -99,7 +99,7 @@ class ContributionForm(forms.ModelForm, BootstrapMixin):
         
         try:
             self.instance.validate_unique(exclude=exclude)
-        except forms.ValidationError ase :
+        except forms.ValidationError as e :
             self._update_errors(e)
 
 
