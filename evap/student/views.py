38fceb201ffc8d3b4d513b05606a01d6fdc2c03f
Johannes (Janno) Wolf <janno42@gmail.com>
2014-07-15 19:34:35 +0200

    added collapsibles for all contributors in a course's questionnaire

--- evap/student/views.py
+++ evap/student/views.py
@@ -7,8 +7,8 @@ from django.utils.datastructures import SortedDict
 from django.utils.translation import ugettext as _
 
 from evap.evaluation.auth import login_required
-from evap.evaluation.models import Course, Semester
-from evap.evaluation.tools import questionnaires_and_contributions
+from evap.evaluation.models import Course, Semester, UserProfile
+from evap.evaluation.tools import questionnaires_and_contributions_by_contributor
 from evap.student.forms import QuestionsForm
 from evap.student.tools import make_form_identifier
 
@@ -42,33 +42,43 @@ def vote(request, course_id):
         raise PermissionDenied
 
     # build forms
-    forms = SortedDict()
-    for questionnaire, contribution in questionnaires_and_contributions(course):
-        form = QuestionsForm(request.POST or None, contribution=contribution, questionnaire=questionnaire)
-        if form.contribution.contributor == request.user:
-            continue # users shall not vote about themselves
-        forms[(contribution, questionnaire)] = form
+    form_group = {}
+    contributors_with_questionnaires = questionnaires_and_contributions_by_contributor(course)
+    for contributor in contributors_with_questionnaires:
+        for questionnaire, contribution in contributors_with_questionnaires[contributor]:
+            form = QuestionsForm(request.POST or None, contribution=contribution, questionnaire=questionnaire)
+            if form.contribution.contributor == request.user:
+                continue # users shall not vote about themselves
+            if not contributor in form_group:
+                form_group[contributor] = SortedDict()
+            form_group[contributor][(contribution, questionnaire)] = form
 
-    if all(form.is_valid() for form in forms.values()):
+    for contributor in form_group:
+        for (contribution, questionnaire), form in form_group[contributor].items():
+            for question in questionnaire.question_set.all():
+                identifier = make_form_identifier(contribution, questionnaire, question)
+
+    if all(form.is_valid() for form in form_group[contributor].values() for contributor in form_group):
         # begin vote operation
         with transaction.commit_on_success():
-            for (contribution, questionnaire), form in forms.items():
-                for question in questionnaire.question_set.all():
-                    identifier = make_form_identifier(contribution, questionnaire, question)
-                    value = form.cleaned_data.get(identifier)
+            for contributor in form_group:
+                for (contribution, questionnaire), form in form_group[contributor].items():
+                    for question in questionnaire.question_set.all():
+                        identifier = make_form_identifier(contribution, questionnaire, question)
+                        value = form.cleaned_data.get(identifier)
 
-                    if type(value) in [str, unicode]:
-                        value = value.strip()
+                        if type(value) in [str, unicode]:
+                            value = value.strip()
 
-                    if value == 6: #no answer
-                        value = None
+                        if value == 6: #no answer
+                            value = None
 
-                    # store the answer if one was given
-                    if value:
-                        question.answer_class.objects.create(
-                            contribution=contribution,
-                            question=question,
-                            answer=value)
+                        # store the answer if one was given
+                        if value:
+                            question.answer_class.objects.create(
+                                contribution=contribution,
+                                question=question,
+                                answer=value)
 
             # remember that the user voted already
             course.voters.add(request.user)
@@ -76,8 +86,21 @@ def vote(request, course_id):
         messages.add_message(request, messages.INFO, _("Your vote was recorded."))
         return redirect('evap.student.views.index')
     else:
+        course_forms = []
+        contributor_questionnaires = {}
+
+        for form in form_group[None].values():
+            print form
+            course_forms.append(form)
+
+        for contributor in form_group:
+            if contributor != None:
+                user_profile = UserProfile.get_for_user(contributor)
+                contributor_questionnaires[user_profile] = form_group[contributor].values()
+
         return render_to_response(
             "student_vote.html",
-            dict(forms=forms.values(),
+            dict(course_forms=course_forms,
+                 contributor_questionnaires=contributor_questionnaires,
                  course=course),
             context_instance=RequestContext(request))
