577b4d4d94863bf702509c9428874bff9de1faa9
johannes.linke <johannes.linke@gmx.de>
2015-01-01 18:11:46 +0100

    re-add django.core.context_processors.request to settings. i was wrong, we are using it.

--- evap/settings.py
+++ evap/settings.py
@@ -139,6 +139,16 @@ MIDDLEWARE_CLASSES = (
     'evap.evaluation.403.Django403Middleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+)
+
 AUTHENTICATION_BACKENDS = (
     'evap.evaluation.auth.RequestAuthUserBackend',
     'django.contrib.auth.backends.ModelBackend',
