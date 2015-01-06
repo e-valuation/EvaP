41ce3d9ee701dce3409a835675b32b4762572d71
johannes.linke <johannes.linke@gmx.de>
2015-01-06 21:06:43 +0100

    don't repeat the js code that makes tables sortable

--- evap/evaluation/templatetags/evaluation_templatetags.py
+++ evap/evaluation/templatetags/evaluation_templatetags.py
@@ -7,3 +7,9 @@ register = Library()
 @register.inclusion_tag("user_list_with_links.html")
 def include_user_list_with_links(users):
     return dict(users=users)
+
+
+@register.inclusion_tag("sortable_form_js.html")
+def include_sortable_form_js():
+    return dict()
+
