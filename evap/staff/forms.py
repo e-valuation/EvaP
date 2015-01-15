9f9044df438ffe03fd3e395abee8392b53b31238
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2015-01-15 22:31:38 +0100

    fix hidden field bug in form.as_div

--- evap/staff/forms.py
+++ evap/staff/forms.py
@@ -140,12 +140,13 @@ class CourseEmailForm(forms.Form, BootstrapMixin):
 
 
 class QuestionnaireForm(forms.ModelForm, BootstrapMixin):
+    def __init__(self, *args, **kwargs):
+        super(QuestionnaireForm, self).__init__(*args, **kwargs)
+        self.fields["index"].widget = forms.HiddenInput()
+
     class Meta:
         model = Questionnaire
-        fields = "__all__"
-        widgets = {
-            'index': forms.HiddenInput(),
-        }
+        exclude = ()
 
 
 class ReviewTextAnswerForm(forms.ModelForm, BootstrapMixin):
