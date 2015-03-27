a3c641753e84d8ca4497c0a651cfa92b62faed78
Johannes Wolf <janno42@gmail.com>
2015-03-28 00:05:23 +0100

    update contributor index page,
    add describing tooltips for the states,
    remove preview button for non-editors,
    add infobox to preview page,
    disable inputs on preview page

--- evap/student/templatetags/student_templatetags.py
+++ evap/student/templatetags/student_templatetags.py
@@ -3,5 +3,8 @@ from django.template import Library
 register = Library()
 
 @register.inclusion_tag('student_vote_questionnaire_group.html')
-def include_student_vote_questionnaire_group(questionnaire_group):
-	return {'questionnaire_group': questionnaire_group}
+def include_student_vote_questionnaire_group(questionnaire_group, preview):
+    return {
+        'questionnaire_group': questionnaire_group,
+        'preview': preview,
+    }
