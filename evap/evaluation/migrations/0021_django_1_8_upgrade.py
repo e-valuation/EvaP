8714ba80259a0e981fa8c605425e7eb2dd8ec060
johannes.linke <johannes.linke@gmx.de>
2015-04-06 11:42:40 +0200

    add a migration

---
+++ evap/evaluation/migrations/0021_django_1_8_upgrade.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('evaluation', '0020_change_vote_date_texts'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userprofile',
+            name='groups',
+            field=models.ManyToManyField(related_name='user_set', to='auth.Group', blank=True, related_query_name='user', verbose_name='groups', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='last_login',
+            field=models.DateTimeField(blank=True, verbose_name='last login', null=True),
+        ),
+    ]
