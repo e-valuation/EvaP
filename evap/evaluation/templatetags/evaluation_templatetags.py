0f9e3a5e0592ae61c7a94f6fa969c9077a453911
Johannes Wolf <janno42@gmail.com>
2015-04-06 23:43:53 +0200

    fix #554; add colors to average grades on results page

--- evap/evaluation/templatetags/evaluation_templatetags.py
+++ evap/evaluation/templatetags/evaluation_templatetags.py
@@ -15,3 +15,7 @@ def include_sortable_form_js():
 @register.inclusion_tag("progress_bar.html")
 def include_progress_bar(done, total, large=False):
     return dict(done=done, total=total, large=large)
+
+@register.inclusion_tag("result_bar.html")
+def include_result_bar(result, show_grades, questionnaire_warning=False):
+    return dict(result=result, show_grades=show_grades, questionnaire_warning=questionnaire_warning)
