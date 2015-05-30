0e2de42cf4c3fa80b53c2735656a6dbe987611e3
Johannes Linke <johannes.linke@student.hpi.de>
2015-05-30 18:51:48 +0200

    introduce model_mommy and use it for tests in the evaluation app

--- evap/evaluation/tests.py
+++ evap/evaluation/tests.py
@@ -1,12 +1,17 @@
 from django_webtest import WebTest
 from django.core import mail
+from django.contrib.auth.hashers import make_password
+from evap.evaluation.models import UserProfile
+from model_mommy import mommy
+
+from datetime import date, timedelta
 
 
 class LoginTests(WebTest):
-    fixtures = ['login_test_data']
 
     def test_passworduser_login(self):
         """ Tests whether a user can login with an incorrect and a correct password. """
+        mommy.make(UserProfile, username='password.user', password=make_password('evap'))
         response = self.app.get("/")
         passwordForm = response.forms[2]
         passwordForm['username'] = 'password.user'
@@ -18,6 +23,8 @@ class LoginTests(WebTest):
     def test_loginkey_login(self):
         """ Tests whether entering a wrong, an expired and a correct login key
             results in the correct return codes. """
+        mommy.make(UserProfile, login_key=12345, login_key_valid_until=date.today() + timedelta(1))
+        mommy.make(UserProfile, login_key=12346, login_key_valid_until=date.today() - timedelta(1))
         response = self.app.get("/")
         loginkeyForm = response.forms[3]
         loginkeyForm['login_key'] = 1111111
@@ -31,11 +38,12 @@ class LoginTests(WebTest):
         """ Tests whether requesting a new login key is only possible for existing users,
             shows the expected success message and sends only one email to the requesting
             user without people in cc even if the user has delegates and cc users. """
+        mommy.make(UserProfile, email='asdf@example.com')
         response = self.app.get("/")
         emailForm = response.forms[4]
-        emailForm['email'] = "asdf@example.com"
+        emailForm['email'] = "doesnotexist@example.com"
         self.assertIn("No user with this email address was found", emailForm.submit())
-        email = "expiredloginkey.user@example.com"
+        email = "asdf@example.com"
         emailForm['email'] = email
         self.assertIn("Successfully sent", emailForm.submit())
         self.assertEqual(len(mail.outbox), 1)
