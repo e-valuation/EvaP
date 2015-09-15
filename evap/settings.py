675c36ad3f748413574699b0d333f505f7752563
Johannes Linke <johannes.linke@posteo.de>
2015-09-15 17:01:04 +0200

    Use the cached template loader only if DEBUG == False

--- evap/settings.py
+++ evap/settings.py
@@ -192,6 +192,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, "templates"),
         ],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 "django.contrib.auth.context_processors.auth",
@@ -205,15 +206,18 @@ TEMPLATES = [
                 "evap.context_processors.legal_notice_active",
                 "evap.context_processors.tracker_url",
             ],
-            'loaders': [
-                ('django.template.loaders.cached.Loader', [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                ]),
-            ],
         },
     },
 ]
+# enable cached template loader when DEBUG == False
+if not DEBUG:
+    TEMPLATES[0]['APP_DIRS'] = False
+    TEMPLATES[0]['OPTIONS']['loaders'] = [
+        ('django.template.loaders.cached.Loader', [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ]),
+    ]
 
 AUTHENTICATION_BACKENDS = (
     'evap.evaluation.auth.RequestAuthUserBackend',
