8fabf3a248bf0310ffc5967ec1209a9df851dd4b
johannes.wolf <johannes.wolf@student.hpi.de>
2015-09-22 15:33:52 +0200

    fix #627: allow graders to confirm that grades have been submitted but will not be updated

--- evap/grades/urls.py
+++ evap/grades/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r"^semester/(\d+)/course/(\d+)/upload$", upload_grades, name="upload_grades"),
     url(r"^semester/(\d+)/course/(\d+)/edit/(\d+)$", edit_grades, name="edit_grades"),
     url(r"^semester/(\d+)/course/(\d+)/delete/(\d+)$", delete_grades, name="delete_grades"),
+    url(r"^semester/(\d+)/course/(\d+)/togglenogrades$", toggle_no_grades, name="toggle_no_grades"),
 ]
