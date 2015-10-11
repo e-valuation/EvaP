1b1891d31a513f7fb437a6cad130aa035a90f8f1
Johannes Wolf <janno42@gmail.com>
2015-10-11 12:09:41 +0200

    use templatetags for choice buttons and don't show responsible option for editors

--- evap/evaluation/templatetags/evaluation_templatetags.py
+++ evap/evaluation/templatetags/evaluation_templatetags.py
@@ -7,7 +7,6 @@ register = Library()
 def include_user_list_with_links(users):
     return dict(users=users)
 
-
 @register.inclusion_tag("sortable_form_js.html")
 def include_sortable_form_js():
     return dict()
@@ -19,3 +18,15 @@ def include_progress_bar(done, total, large=False):
 @register.inclusion_tag("result_bar.html")
 def include_result_bar(result, show_grades, questionnaire_warning=False):
     return dict(result=result, show_grades=show_grades, questionnaire_warning=questionnaire_warning)
+
+@register.inclusion_tag("responsibility_buttons.html")
+def include_responsibility_buttons(form, can_change_responsible=False):
+    return dict(form=form, can_change_responsible=can_change_responsible)
+
+@register.inclusion_tag("comment_visibility_buttons.html")
+def include_comment_visibility_buttons(form):
+    return dict(form=form)
+
+@register.inclusion_tag("choice_button.html")
+def include_choice_button(formelement, choice, enabled, tooltip):
+    return dict(formelement=formelement, choice=choice, enabled=enabled, tooltip=tooltip)
