54fa019ae20614477a8b104eed6fd235c4e99e9b
Johannes Wolf <janno42@gmail.com>
2016-02-13 23:23:58 +0100

    fix #732; delete navbar cache when creating or deleting semester

--- evap/staff/tools.py
+++ evap/staff/tools.py
@@ -1,5 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
+
+from evap.evaluation.models import UserProfile
 
 import urllib.parse
 
@@ -8,3 +12,11 @@ def custom_redirect(url_name, *args, **kwargs):
     url = reverse(url_name, args=args)
     params = urllib.parse.urlencode(kwargs)
     return HttpResponseRedirect(url + "?%s" % params)
+
+def delete_navbar_cache():
+    # delete navbar cache from base.html
+    for user in UserProfile.objects.all():
+        key = make_template_fragment_key('navbar', [user.username, 'de'])
+        cache.delete(key)
+        key = make_template_fragment_key('navbar', [user.username, 'en'])
+        cache.delete(key)
