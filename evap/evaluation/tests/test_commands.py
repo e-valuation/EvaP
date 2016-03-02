53acb3c9e8e52a5f00bec5c266cc7ee4bcc81629
David Schumann <david.schumann@student.hpi.de>
2016-03-02 16:46:49 +0100

    Added tests for reload_testdata and refactored print calls there

--- evap/evaluation/tests/test_commands.py
+++ evap/evaluation/tests/test_commands.py
@@ -12,6 +12,31 @@ from unittest.mock import patch
 from evap.evaluation.models import UserProfile, Course
 
 
+class TestReloadTestdataCommand(TestCase):
+    @patch('builtins.input')
+    @patch('evap.evaluation.management.commands.reload_testdata.call_command')
+    def test_aborts(self, mock_call_command, mock_input):
+        mock_input.return_value = 'not yes'
+
+        management.call_command('reload_testdata', stdout=StringIO())
+
+        self.assertEqual(mock_call_command.call_count, 0)
+
+    @patch('builtins.input')
+    @patch('evap.evaluation.management.commands.reload_testdata.call_command')
+    def test_executes_key_commands(self, mock_call_command, mock_input):
+        mock_input.return_value = 'yes'
+
+        management.call_command('reload_testdata', stdout=StringIO())
+
+        mock_call_command.assert_any_call('reset_db', user='evap', interactive=False)
+        mock_call_command.assert_any_call('migrate')
+        mock_call_command.assert_any_call('createcachetable')
+        mock_call_command.assert_any_call('loaddata', 'test_data')
+
+        self.assertEqual(mock_call_command.call_count, 4)
+
+
 class TestRefreshResultsCacheCommand(TestCase):
     def test_calls_calculate_results(self):
         mommy.make(Course)
