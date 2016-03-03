7a902987daa7365ad28471c37709555b2d759ac7
David Schumann <david.schumann@student.hpi.de>
2016-03-03 21:13:48 +0100

    Test that exporter returns an excel file

--- evap/staff/tests/test_views.py
+++ evap/staff/tests/test_views.py
@@ -1,13 +1,39 @@
 import os
 
+import xlrd
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.test.utils import override_settings
 from django_webtest import WebTest
 from model_mommy import mommy
 
-from evap.evaluation.models import Semester, UserProfile, Course
-from evap.evaluation.tests.test_utils import lastform
+from evap.evaluation.models import Semester, UserProfile, Course, CourseType
+from evap.evaluation.tests.test_utils import lastform, ViewTest
+
+
+class TestSemesterExportView(ViewTest):
+    url = '/staff/semester/1/export'
+    users = ['staff']
+
+    def setUp(self):
+        self.semester = mommy.make(Semester)
+        self.course_type = mommy.make(CourseType)
+        self.course = mommy.make(Course, type=self.course_type, semester=self.semester)
+        mommy.make(UserProfile, username="staff", groups=[Group.objects.get(name="Staff")])
+
+    def test_view_downloads_excel_file(self):
+        page = self.app.get('/staff/semester/1/export', user='staff')
+        form = lastform(page)
+
+        # Check one course type.
+        form.set('form-0-selected_course_types', 'id_form-0-selected_course_types_0')
+
+        response = form.submit()
+
+        # Load response as Excel file and check its heading for correctness.
+        workbook = xlrd.open_workbook(file_contents=response.content)
+        self.assertEquals(workbook.sheets()[0].row_values(0)[0],
+                          'Evaluation {0}\n\n{1}'.format(self.semester.name, ", ".join([self.course_type.name])))
 
 
 @override_settings(INSTITUTION_EMAIL_DOMAINS=["institution.com", "student.institution.com"])
@@ -15,11 +41,11 @@ class TestSemesterCourseImportParticipantsView(WebTest):
     @classmethod
     def setUpTestData(cls):
         mommy.make(Semester, pk=1)
-        mommy.make(UserProfile, username="user", groups=[Group.objects.get(name="Staff")])
+        mommy.make(UserProfile, username="staff", groups=[Group.objects.get(name="Staff")])
         cls.course = mommy.make(Course, pk=1)
 
     def test_import_valid_file(self):
-        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='user')
+        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='staff')
 
         original_participant_count = self.course.participants.count()
 
@@ -30,7 +56,7 @@ class TestSemesterCourseImportParticipantsView(WebTest):
         self.assertEqual(self.course.participants.count(), original_participant_count + 2)
 
     def test_import_invalid_file(self):
-        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='user')
+        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='staff')
 
         original_user_count = UserProfile.objects.count()
 
@@ -45,7 +71,7 @@ class TestSemesterCourseImportParticipantsView(WebTest):
         self.assertEquals(UserProfile.objects.count(), original_user_count)
 
     def test_test_run(self):
-        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='user')
+        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='staff')
 
         original_participant_count = self.course.participants.count()
 
@@ -56,7 +82,7 @@ class TestSemesterCourseImportParticipantsView(WebTest):
         self.assertEqual(self.course.participants.count(), original_participant_count)
 
     def test_suspicious_operation(self):
-        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='user')
+        page = self.app.get("/staff/semester/1/course/1/importparticipants", user='staff')
 
         form = lastform(page)
         form["excel_file"] = (os.path.join(settings.BASE_DIR, "staff/fixtures/valid_user_import.xls"),)
