6a7331339ccd1642ddd9b35f89cb0fefe00288f7
David Schumann <david.schumann@student.hpi.de>
2016-03-10 00:55:59 +0100

    UI modal popup for submitting Feedback or Questions

---
+++ evap/static/js/feedback_form.js
@@ -0,0 +1,19 @@
+$('#feedback-form').on('submit', function(event){
+
+    event.preventDefault();
+    $('#feedback-spinner').show();
+
+    email = $('#sender-email').val();
+    message = $('#message-text').val();
+
+    $.ajax({
+        url : "/feedback/create",
+        type : "POST",
+        data : { sender_email : email, message: message },
+
+        success : function(json) {
+            $('#feedback-spinner').hide();
+            $('#feedback-modal').modal('hide');
+            $('#feedback-button').html('Message Sent!');
+        }});
+});
