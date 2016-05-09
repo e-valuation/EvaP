d4b8615b1ea752ce2c5391623dc6dc3561a87b10
Johannes Linke <johannes.linke@student.hpi.de>
2016-05-09 19:27:06 +0200

    Update update.sh, fixes #787

---
+++ deployment/update_production.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e # abort on error
+cd `dirname $0`/.. # change to root directory
+set -x # print executed commands
+
+sudo -u evap git fetch
+sudo -u evap git checkout origin/release
+sudo pip3 install -r requirements.txt
+sudo -u evap ./manage.py migrate
+sudo -u evap ./manage.py collectstatic --noinput
+sudo -u evap ./manage.py compilemessages
+sudo -u evap ./manage.py compress --verbosity=0
+sudo -u evap ./manage.py clear_cache
+sudo -u evap ./manage.py refresh_results_cache
+sudo service apache2 restart
+
+{ set +x; } 2>/dev/null # don't print the echo command, and don't print the 'set +x' itself
+
+echo Update completed.
