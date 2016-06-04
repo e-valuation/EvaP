8cf0e5aff95a5ad132182d92168978d610dab311
Johannes Linke <johannes.linke@posteo.de>
2016-06-04 10:54:29 +0200

    Use normal template includes instead of custom inclusion tags

--- evap/staff/templatetags/staff_templatetags.py
+++ evap/staff/templatetags/staff_templatetags.py
@@ -8,18 +8,3 @@ register = Library()
 @register.inclusion_tag("staff_semester_menu.html")
 def include_staff_semester_menu():
     return dict(semesters=Semester.objects.all()[:5])
-
-
-@register.inclusion_tag('staff_user_labels.html')
-def include_staff_user_labels(user):
-    return dict(user=user)
-
-
-@register.inclusion_tag("staff_questionnaire_index_list.html")
-def include_staff_questionnaire_index_list(questionnaires, headline):
-    return dict(questionnaires=questionnaires, headline=headline)
-
-
-@register.inclusion_tag("staff_semester_view_course.html")
-def include_staff_semester_view_course(semester, course, disable_if_archived, info_only=False):
-    return dict(semester=semester, course=course, disable_if_archived=disable_if_archived, info_only=info_only)
