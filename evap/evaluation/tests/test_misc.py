bf0cd937b3a4a2bd1ee011e3ce245409c05ab61e
Johannes Linke <johannes.linke@student.hpi.de>
2016-08-15 22:12:19 +0200

    Use the new check parameter of makemigrations in tests

--- evap/evaluation/tests/test_misc.py
+++ evap/evaluation/tests/test_misc.py
@@ -66,9 +66,6 @@ class TestMissingMigrations(TestCase):
     def test_for_missing_migrations(self):
         output = StringIO()
         try:
-            call_command('makemigrations', interactive=False, dry_run=True, exit_code=True, stdout=output)
-        except SystemExit as e:
-            # The exit code will be 1 when there are no missing migrations
-            self.assertEqual(str(e), '1')
-        else:
-            self.fail("There are missing migrations:\n %s" % output.getvalue())
+            call_command('makemigrations', dry_run=True, check=True, stdout=output)
+        except SystemExit:
+            self.fail("There are model changes not reflected in migrations, please run makemigrations.")
