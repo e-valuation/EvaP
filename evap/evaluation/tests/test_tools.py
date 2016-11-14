f5bd1c5c9c2ae1e5162d22541bd2174fc149ae46
Richard <klavier22@hotmail.de>
2016-11-14 22:55:44 +0100

    Issue #751: Tests for the calculated results for a course.

--- evap/evaluation/tests/test_tools.py
+++ evap/evaluation/tests/test_tools.py
@@ -1,9 +1,10 @@
 from django.core.cache import cache
 from django.test.testcases import TestCase
+from django.test import override_settings
 from model_mommy import mommy
 
-from evap.evaluation.models import Course
-from evap.evaluation.tools import calculate_results
+from evap.evaluation.models import Course, UserProfile, Contribution, RatingAnswerCounter, Questionnaire, Question
+from evap.evaluation.tools import calculate_results, get_answers, get_answers_from_answer_counters, calculate_average_grades_and_deviation
 
 
 class TestCalculateResults(TestCase):
@@ -15,3 +16,114 @@ class TestCalculateResults(TestCase):
         calculate_results(course)
 
         self.assertIsNotNone(cache.get('evap.staff.results.tools.calculate_results-{:d}'.format(course.id)))
+
+    def test_calculation_results(self):
+        contributor1 = mommy.make(UserProfile)
+        student = mommy.make(UserProfile)
+
+        course = mommy.make(Course, state='published', participants=[student, contributor1])
+        questionnaire = mommy.make(Questionnaire)
+        question = mommy.make(Question, questionnaire=questionnaire, type="G")
+        contribution1 = mommy.make(Contribution, contributor=contributor1, course=course, questionnaires=[questionnaire])
+
+        mommy.make(RatingAnswerCounter, question=question, contribution=contribution1, answer=1, count=5)
+        mommy.make(RatingAnswerCounter, question=question, contribution=contribution1, answer=2, count=15)
+        mommy.make(RatingAnswerCounter, question=question, contribution=contribution1, answer=3, count=40)
+        mommy.make(RatingAnswerCounter, question=question, contribution=contribution1, answer=4, count=60)
+        mommy.make(RatingAnswerCounter, question=question, contribution=contribution1, answer=5, count=30)
+
+        results = calculate_results(course)
+
+        self.assertTrue(len(results) == 1)
+        self.assertTrue(len(results[0].results) == 1)
+        result = results[0].results[0]
+
+        self.assertEquals(result.total_count, 150)
+        self.assertAlmostEqual(result.average, float(109) / 30)
+        self.assertAlmostEqual(result.deviation, 1.015983376941878)
+
+    def test_answer_counting(self):
+        contributor1 = mommy.make(UserProfile)
+        contributor2 = mommy.make(UserProfile)
+        student = mommy.make(UserProfile)
+
+        course1 = mommy.make(Course, state='published', participants=[student, contributor1])
+        questionnaire = mommy.make(Questionnaire)
+        question1 = mommy.make(Question, questionnaire=questionnaire, type="G")
+        question2 = mommy.make(Question, questionnaire=questionnaire, type="G")
+        contribution1 = mommy.make(Contribution, contributor=contributor1, course=course1, questionnaires=[questionnaire])
+        contribution2 = mommy.make(Contribution, contributor=contributor1, questionnaires=[questionnaire])
+        contribution3 = mommy.make(Contribution, contributor=contributor2, course=course1, questionnaires=[questionnaire])
+
+        rating_answer_counters = []
+        rating_answer_counters.append(mommy.make(RatingAnswerCounter, question=question1, contribution=contribution1, answer=1, count=1))
+        rating_answer_counters.append(mommy.make(RatingAnswerCounter, question=question1, contribution=contribution1, answer=3, count=4))
+        rating_answer_counters.append(mommy.make(RatingAnswerCounter, question=question1, contribution=contribution1, answer=4, count=2))
+        rating_answer_counters.append(mommy.make(RatingAnswerCounter, question=question1, contribution=contribution1, answer=5, count=3))
+
+        mommy.make(RatingAnswerCounter, question=question1, contribution=contribution2, answer=1, count=1)
+        mommy.make(RatingAnswerCounter, question=question1, contribution=contribution3, answer=1, count=1)
+        mommy.make(RatingAnswerCounter, question=question2, contribution=contribution1, answer=1, count=1)
+
+        answer_counters = get_answers(contribution1, question1)
+        self.assertSetEqual(set(rating_answer_counters), set(answer_counters))
+
+        answers = get_answers_from_answer_counters(answer_counters)
+        self.assertListEqual(answers, [1, 3, 3, 3, 3, 4, 4, 5, 5, 5])
+
+    @override_settings(CONTRIBUTION_PERCENTAGE=0.3, GRADE_PERCENTAGE=0.6)
+    def test_average_grades(self):
+        contributor1 = mommy.make(UserProfile)
+        contributor2 = mommy.make(UserProfile)
+
+        course = mommy.make(Course)
+        questionnaire = mommy.make(Questionnaire)
+        question_grade = mommy.make(Question, questionnaire=questionnaire, type="G")
+        question_likert = mommy.make(Question, questionnaire=questionnaire, type="L")
+        general_contribution = mommy.make(Contribution, contributor=None, course=course, questionnaires=[questionnaire])
+        contribution1 = mommy.make(Contribution, contributor=contributor1, course=course, questionnaires=[questionnaire])
+        contribution2 = mommy.make(Contribution, contributor=contributor2, course=course, questionnaires=[questionnaire])
+
+        mommy.make(RatingAnswerCounter, question=question_grade, contribution=contribution1, answer=1, count=1)
+        mommy.make(RatingAnswerCounter, question=question_grade, contribution=contribution2, answer=4, count=2)
+        mommy.make(RatingAnswerCounter, question=question_likert, contribution=contribution1, answer=3, count=4)
+        mommy.make(RatingAnswerCounter, question=question_likert, contribution=general_contribution, answer=5, count=3)
+
+        total_likert = 0.3 * 3 + (1 - 0.3) * 5
+        total_grade = 2.5
+        total = 0.6 * total_grade + (1 - 0.6) * total_likert
+
+        average, deviation = calculate_average_grades_and_deviation(course)
+
+        self.assertAlmostEqual(average, total)
+        self.assertAlmostEqual(deviation, 0)
+
+    @override_settings(CONTRIBUTION_PERCENTAGE=0.3, GRADE_PERCENTAGE=0.6)
+    def test_average_deviation(self):
+        contributor1 = mommy.make(UserProfile)
+        contributor2 = mommy.make(UserProfile)
+
+        course = mommy.make(Course)
+        questionnaire = mommy.make(Questionnaire)
+        question_grade = mommy.make(Question, questionnaire=questionnaire, type="G")
+        question_likert = mommy.make(Question, questionnaire=questionnaire, type="L")
+        general_contribution = mommy.make(Contribution, contributor=None, course=course, questionnaires=[questionnaire])
+        contribution1 = mommy.make(Contribution, contributor=contributor1, course=course, questionnaires=[questionnaire])
+        contribution2 = mommy.make(Contribution, contributor=contributor2, course=course, questionnaires=[questionnaire])
+
+        mommy.make(RatingAnswerCounter, question=question_grade, contribution=contribution1, answer=1, count=1)
+        mommy.make(RatingAnswerCounter, question=question_grade, contribution=contribution1, answer=3, count=1)
+        mommy.make(RatingAnswerCounter, question=question_grade, contribution=contribution2, answer=4, count=2)
+        mommy.make(RatingAnswerCounter, question=question_grade, contribution=contribution2, answer=2, count=2)
+        mommy.make(RatingAnswerCounter, question=question_likert, contribution=contribution1, answer=3, count=4)
+        mommy.make(RatingAnswerCounter, question=question_likert, contribution=contribution1, answer=5, count=4)
+        mommy.make(RatingAnswerCounter, question=question_likert, contribution=general_contribution, answer=5, count=3)
+
+        deviation = calculate_average_grades_and_deviation(course)[1]
+
+        total_likert_dev = 0.3 * 1 + 0.7 * 0
+        total_grade_dev = 1
+        total_dev = 0.6 * total_grade_dev + 0.4 * total_likert_dev
+
+        self.assertAlmostEqual(deviation, total_dev)
+
