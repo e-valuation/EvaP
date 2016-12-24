88f4aa1011c4cbb22283a0b6ae9fa01097a76e6c
Johannes Linke <johannes.linke@posteo.de>
2016-12-24 12:30:31 +0100

    Move base template and error pages into evaluation app

--- evap/settings.py
+++ evap/settings.py
@@ -200,9 +200,6 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, "templates"),
-        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
