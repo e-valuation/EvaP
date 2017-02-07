0d8e59ebb4700c78d789680fd6ac5cb163737f1c
Johannes Linke <johannes.linke@posteo.de>
2017-02-07 11:01:38 +0100

    Introduce @log_exceptions and use it on two management commands

---
+++ evap/evaluation/management/commands/tools.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def log_exceptions(cls):
+    """
+    This decorator is meant to decorate management commands.
+    Any exceptions raised in the command's handle method will be logged and re-raised.
+    """
+    class NewClass(cls):
+        def handle(self, *args, **options):
+            try:
+                super().handle(args, options)
+            except Exception:
+                logger.exception("Management command '{}' failed. Traceback follows: ".format(sys.argv[1]))
+                raise
+    return NewClass
