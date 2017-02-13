e15d5114360a062bab7da6e4f550a4d17fa48340
Johannes Wolf <johannes.wolf@hpi.de>
2017-02-13 18:25:59 +0100

    fix #911; enable reduced staff semester view for reviewers

--- evap/staff/templatetags/staff_templatetags.py
+++ evap/staff/templatetags/staff_templatetags.py
@@ -6,5 +6,8 @@ register = Library()
 
 
 @register.inclusion_tag("staff_semester_menu.html")
-def include_staff_semester_menu():
-    return dict(semesters=Semester.objects.all()[:5])
+def include_staff_semester_menu(indentation=False):
+    return {
+        'semesters': Semester.objects.all()[:5],
+        'indentation': indentation,
+    }
