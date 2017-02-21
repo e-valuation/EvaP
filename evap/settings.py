32a2431b4a20480184a57e791386fd626beb31bf
Johannes Linke <johannes.linke@posteo.de>
2017-02-21 15:22:46 +0100

    Don't explicitly enable the cached template loader, that's default now

--- evap/settings.py
+++ evap/settings.py
@@ -220,15 +220,6 @@ TEMPLATES = [
         },
     },
 ]
-# enable cached template loader when DEBUG == False
-if not DEBUG:
-    TEMPLATES[0]['APP_DIRS'] = False
-    TEMPLATES[0]['OPTIONS']['loaders'] = [
-        ('django.template.loaders.cached.Loader', [
-            'django.template.loaders.filesystem.Loader',
-            'django.template.loaders.app_directories.Loader',
-        ]),
-    ]
 
 AUTHENTICATION_BACKENDS = [
     'evap.evaluation.auth.RequestAuthUserBackend',
