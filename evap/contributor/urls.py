ca1fc12d7439e62eca3cf09738d2ee236aa629bf
Johannes Linke <johannes.linke@posteo.de>
2017-02-23 15:39:59 +0100

    Remove wildcard imports

--- evap/contributor/urls.py
+++ evap/contributor/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
-from evap.contributor.views import index, settings_edit, course_view, course_edit, course_preview
+from evap.contributor import views
 
 
 app_name = "contributor"
 
 urlpatterns = [
-    url(r"^$", index, name="index"),
-    url(r"^settings$", settings_edit, name="settings_edit"),
-    url(r"^course/(\d+)$", course_view, name="course_view"),
-    url(r"^course/(\d+)/edit$", course_edit, name="course_edit"),
-    url(r"^course/(\d+)/preview$", course_preview, name="course_preview"),
+    url(r"^$", views.index, name="index"),
+    url(r"^settings$", views.settings_edit, name="settings_edit"),
+    url(r"^course/(\d+)$", views.course_view, name="course_view"),
+    url(r"^course/(\d+)/edit$", views.course_edit, name="course_edit"),
+    url(r"^course/(\d+)/preview$", views.course_preview, name="course_preview"),
 ]
