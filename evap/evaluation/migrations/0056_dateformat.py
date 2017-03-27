e6530d2f6677d043ce5025145358227b6042e2a6
Nico Scordialo <nicoscordialo@googlemail.com>
2017-03-27 18:45:16 +0200

    rebased migrations

--- evap/evaluation/migrations/0056_dateformat.py
+++ evap/evaluation/migrations/0056_dateformat.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', '0049_rename_course_states'),
+        ('evaluation', '0055_reviewer_group'),
     ]
 
     operations = [
