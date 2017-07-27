d24c675c7a0b1539c1f6ed4e9fb995926a3e8640
Johannes Wolf <janno42@gmail.com>
2017-07-27 22:07:02 +0200

    further navbar and footer changes

--- evap/evaluation/templatetags/navbar_templatetags.py
+++ evap/evaluation/templatetags/navbar_templatetags.py
@@ -7,7 +7,7 @@ register = Library()
 @register.inclusion_tag("navbar.html")
 def include_navbar(user):
     return {
-    	'user': user,
-    	'result_semesters': Semester.get_all_with_published_courses(),
-    	'last_five_semesters': Semester.objects.all()[:5],
+        'user': user,
+        'result_semesters': Semester.get_all_with_published_courses(),
+        'last_five_semesters': Semester.objects.all()[:5],
     }
