1d71e4efcd34d01f28e30c6026c8dcc708921193
Philipp Fischbeck <philipp.fischbeck@student.hpi.de>
2017-08-21 18:22:36 +0200

    Add positive and negative yes-no questions, fixes #345 (#997)
    * Add positive and negative yes-no questions
    
    * Add questionnaire and random answers for new question types to test_data.json

--- evap/student/forms.py
+++ evap/student/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
 from evap.student.tools import make_form_identifier
-from evap.evaluation.tools import LIKERT_NAMES, GRADE_NAMES
+from evap.evaluation.tools import LIKERT_NAMES, GRADE_NAMES, POSITIVE_YES_NO_NAMES, NEGATIVE_YES_NO_NAMES
 
 
 LIKERT_CHOICES = [(str(k), v) for k, v in LIKERT_NAMES.items()]
 GRADE_CHOICES = [(str(k), v) for k, v in GRADE_NAMES.items()]
+POSITIVE_YES_NO_CHOICES = [(str(k), v) for k, v in POSITIVE_YES_NO_NAMES.items()]
+NEGATIVE_YES_NO_CHOICES = [(str(k), v) for k, v in NEGATIVE_YES_NO_NAMES.items()]
 
 
 class QuestionsForm(forms.Form):
@@ -32,6 +34,16 @@ class QuestionsForm(forms.Form):
             elif question.is_grade_question:
                 field = forms.TypedChoiceField(widget=forms.RadioSelect(),
                                                choices=GRADE_CHOICES,
+                                               coerce=int,
+                                               **field_args)
+            elif question.is_positive_yes_no_question:
+                field = forms.TypedChoiceField(widget=forms.RadioSelect(),
+                                               choices=POSITIVE_YES_NO_CHOICES,
+                                               coerce=int,
+                                               **field_args)
+            elif question.is_negative_yes_no_question:
+                field = forms.TypedChoiceField(widget=forms.RadioSelect(),
+                                               choices=NEGATIVE_YES_NO_CHOICES,
                                                coerce=int,
                                                **field_args)
 
