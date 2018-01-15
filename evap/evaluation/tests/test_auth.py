e9d88651f2ee20812b380df4e13c743b502abac0
Johannes Wolf <janno42@gmail.com>
2018-01-15 22:36:05 +0100

    Restrict external access (#1085)
    fix #1031; restrict access for external users

--- evap/evaluation/tests/test_auth.py
+++ evap/evaluation/tests/test_auth.py
@@ -3,7 +3,7 @@ from django.core import mail
 
 from model_mommy import mommy
 
-from evap.evaluation.models import UserProfile
+from evap.evaluation.models import Contribution, Course, UserProfile
 from evap.evaluation.tests.tools import WebTest
 
 
@@ -16,27 +16,30 @@ class LoginTests(WebTest):
         cls.external_user.ensure_valid_login_key()
         cls.inactive_external_user = mommy.make(UserProfile, email="inactive@extern.com", is_active=False)
         cls.inactive_external_user.ensure_valid_login_key()
+        course = mommy.make(Course, state='published')
+        mommy.make(Contribution, course=course, contributor=cls.external_user, can_edit=True, responsible=True, comment_visibility=Contribution.ALL_COMMENTS)
+        mommy.make(Contribution, course=course, contributor=cls.inactive_external_user, can_edit=True, responsible=False, comment_visibility=Contribution.ALL_COMMENTS)
 
     def test_login_url_works(self):
-        self.assertRedirects(self.app.get(reverse("results:index")), "/?next=/results/")
+        self.assertRedirects(self.app.get(reverse("contributor:index")), "/?next=/contributor/")
 
-        url_with_key = reverse("results:index") + "?loginkey=%s" % self.external_user.login_key
+        url_with_key = reverse("contributor:index") + "?loginkey=%s" % self.external_user.login_key
         self.app.get(url_with_key)
 
     def test_login_key_valid_only_once(self):
-        page = self.app.get(reverse("results:index") + "?loginkey=%s" % self.external_user.login_key)
+        page = self.app.get(reverse("contributor:index") + "?loginkey=%s" % self.external_user.login_key)
         self.assertContains(page, 'Logged in as ' + self.external_user.full_name)
         page = self.app.get(reverse("django-auth-logout")).follow()
         self.assertContains(page, 'Not logged in')
-        page = self.app.get(reverse("results:index") + "?loginkey=%s" % self.external_user.login_key).follow()
+        page = self.app.get(reverse("contributor:index") + "?loginkey=%s" % self.external_user.login_key).follow()
         self.assertContains(page, 'The login URL is not valid anymore.')
         self.assertEqual(len(mail.outbox), 1)  # a new login key was sent
         self.external_user.refresh_from_db()
-        page = self.app.get(reverse("results:index") + "?loginkey=%s" % self.external_user.login_key)
+        page = self.app.get(reverse("contributor:index") + "?loginkey=%s" % self.external_user.login_key)
         self.assertContains(page, 'Logged in as ' + self.external_user.full_name)
 
     def test_inactive_external_users_can_not_login(self):
-        page = self.app.get(reverse("results:index") + "?loginkey=%s" % self.inactive_external_user.login_key).follow()
+        page = self.app.get(reverse("contributor:index") + "?loginkey=%s" % self.inactive_external_user.login_key).follow()
         self.assertContains(page, "Inactive users are not allowed to login")
         self.assertNotContains(page, "Logged in")
 
