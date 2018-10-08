5459a4ee255c58dd02b20ce2854b1d2c60db9947
Johannes Linke <johannes.linke@posteo.de>
2018-10-08 20:28:52 +0200

    update_production.sh: stop apache at the beginning, start it at the end, fixes #1237
    We noticed that lots of user interactions between the script start and the apache reload could result in crashes, and the backup would not be up to date.

--- deployment/update_production.sh
+++ deployment/update_production.sh
@@ -2,6 +2,10 @@
 set -e # abort on error
 cd `dirname $0`/.. # change to root directory
 
+sudo -H -u evap git fetch
+
+sudo service apache2 stop
+
 # argument 1 is the filename for the backupfile.
 # if no argument is present, no backup will be created.
 if [ $# -eq 1 ] # if there is exactly one argument
@@ -14,19 +18,16 @@ if [ $# -eq 1 ] # if there is exactly one argument
         set -x # print executed commands
 fi
 
-sudo -H -u evap git fetch
 sudo -H -u evap git checkout origin/release
 sudo -H -u evap pip3 install --user -r requirements.txt
 sudo -H -u evap ./manage.py compilemessages
 sudo -H -u evap ./manage.py collectstatic --noinput
 sudo -H -u evap ./manage.py compress --verbosity=0
 sudo -H -u evap ./manage.py migrate
-# reload only after static files are updated, so the new code finds all the files it expects.
-# also, reload after migrations happened. see https://github.com/fsr-itse/EvaP/pull/817 for a discussion.
-sudo service apache2 reload
-# update caches. this can take minutes but doesn't need a reload.
 sudo -H -u evap ./manage.py clear_cache
 sudo -H -u evap ./manage.py refresh_results_cache
+
+sudo service apache2 start
 
 { set +x; } 2>/dev/null # don't print the echo command, and don't print the 'set +x' itself
 
