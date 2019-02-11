94ffa43fac050e805b31b70d9251f28cf8df703d
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2019-02-11 21:57:36 +0100

    Revert "move translation of warning descriptors to template context"
    This reverts commit 70ac2ecb

--- evap/staff/templatetags/staff_templatetags.py
+++ evap/staff/templatetags/staff_templatetags.py
@@ -2,11 +2,9 @@ from django.template import Library
 
 from evap.staff.importers import WARNING_DESCRIPTIONS
 
-from django.utils.translation import ugettext_lazy as _
-
 register = Library()
 
 
 @register.filter(name='warningname')
 def warningname(warning):
-    return _(WARNING_DESCRIPTIONS.get(warning))
+    return WARNING_DESCRIPTIONS.get(warning)
