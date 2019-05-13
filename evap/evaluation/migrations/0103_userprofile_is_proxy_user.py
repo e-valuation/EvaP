32f49faba796782a36a265e325a1c6a2c28e4b3b
velramiir <justus.zeinert@student.hpi.de>
2019-05-13 21:29:45 +0200

    Rename migration, because different migration was created in the meantime

--- evap/evaluation/migrations/0103_userprofile_is_proxy_user.py
+++ evap/evaluation/migrations/0103_userprofile_is_proxy_user.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', '0101_evaluation_weight'),
+        ('evaluation', '0102_individual_publishing_notice_email_template'),
     ]
 
     operations = [
