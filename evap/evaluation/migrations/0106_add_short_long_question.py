8142ca4ed815a0985cc3f16796a0da06c556067a
Jannis Berndt <berndtjannis@gmail.com>
2019-12-02 18:13:45 +0100

    Changed Formatting of 0106_add_short_long_question.py

--- evap/evaluation/migrations/0106_add_short_long_question.py
+++ evap/evaluation/migrations/0106_add_short_long_question.py
@@ -13,6 +13,25 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='question',
             name='type',
-            field=models.PositiveSmallIntegerField(choices=[('Text', ((0, 'Text question'),)), ('Unipolar Likert', ((1, 'Agreement question'),)), ('Grade', ((2, 'Grade question'),)), ('Bipolar Likert', ((6, 'Easy-difficult question'), (7, 'Few-many question'), (8, 'Little-much question'), (9, 'Small-large question'), (10, 'Slow-fast question'), (11, 'Short-long question'))), ('Yes-no', ((3, 'Positive yes-no question'), (4, 'Negative yes-no question'))), ('Layout', ((5, 'Heading'),))], verbose_name='question type'),
+            field=models.PositiveSmallIntegerField(
+                choices=[
+                    ('Text', ((0, 'Text question'),)),
+                    ('Unipolar Likert', ((1, 'Agreement question'),)),
+                    ('Grade', ((2, 'Grade question'),)),
+                    ('Bipolar Likert', (
+                        (6, 'Easy-difficult question'),
+                        (7, 'Few-many question'),
+                        (8, 'Little-much question'),
+                        (9, 'Small-large question'),
+                        (10, 'Slow-fast question'),
+                        (11, 'Short-long question'))
+                    ),
+                    ('Yes-no', (
+                        (3, 'Positive yes-no question'),
+                        (4, 'Negative yes-no question'))
+                    ),
+                    ('Layout', ((5, 'Heading'),))
+                ],
+                verbose_name='question type'),
         ),
     ]
