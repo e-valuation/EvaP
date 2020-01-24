884ab52723f56b525e91b7a91012e978ce889ed3
Richard Ebeling <He3lixxx@users.noreply.github.com>
2020-01-24 15:16:24 +0100

    Remove default secret key. Development VMs are set up to automatically generate one. Having a default is a risk.

--- evap/settings.py
+++ evap/settings.py
@@ -94,9 +94,6 @@ ADMINS = [
 # The page URL that is used in email templates.
 PAGE_URL = "localhost:8000"
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'k9-)vh3c_dtm6bpi7j(!*s_^91v0!ekjt_#o&0i$e22tnn^-vb'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # postgresql', 'mysql', 'sqlite3' or 'oracle'.
