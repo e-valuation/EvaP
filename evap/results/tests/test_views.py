61013e0b7a54d5ebb6a8cabfbb5df4345469f220
Johannes Wolf <johannes-wolf@posteo.de>
2020-02-03 15:05:03 +0100

    add line break after intro text in visibility tooltips

--- evap/results/tests/test_views.py
+++ evap/results/tests/test_views.py
@@ -498,7 +498,7 @@ class TestResultsTextanswerVisibility(WebTest):
 
     def test_textanswer_visibility_info_is_shown(self):
         page = self.app.get("/results/semester/1/evaluation/1", user='contributor')
-        self.assertIn("can be seen by: contributor user", page)
+        self.assertRegex(page.body.decode(), r"can be seen by:<br />\s*contributor user")
 
     def test_textanswer_visibility_info_for_proxy_user(self):
         page = self.app.get("/results/semester/1/evaluation/1", user='responsible')
