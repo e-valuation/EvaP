fcc885e89d46d7c4c58c010004b0e184f75c54e0
Felix Rindt <felix@rindt.me>
2020-09-21 20:33:48 +0200

    dont save with model bakery

--- evap/evaluation/tests/test_models_logging.py
+++ evap/evaluation/tests/test_models_logging.py
@@ -14,7 +14,6 @@ class TestLoggedModel(TestCase):
         self.new_start_date = self.old_start_date + timedelta(days=10)
 
         self.course = baker.make(Course)
-        self.course.save()
 
         self.evaluation = baker.make(
             Evaluation,
@@ -30,7 +29,7 @@ class TestLoggedModel(TestCase):
 
         self.logentry = self.evaluation.related_logentries()[1]
 
-    def test_voters_not_in_evluation_data(self):
+    def test_voters_not_in_evaluation_data(self):
         self.assertFalse(any("voters" in entry.data for entry in self.evaluation.related_logentries()))
 
     def test_datetime_change(self):
@@ -58,31 +57,25 @@ class TestLoggedModel(TestCase):
 
     def test_creation(self):
         course = baker.make(Course)
-        course.save()
         self.assertEqual(course.related_logentries().count(), 1)
 
     def test_related_logged_model_creation(self):
         self.assertEqual(self.evaluation.related_logentries().count(), 2)
         contribution = baker.make(Contribution, evaluation=self.evaluation)
-        contribution.save()
         self.assertFalse(contribution.related_logentries().exists())
         self.assertEqual(self.evaluation.related_logentries().count(), 3)
 
     def test_m2m_creation(self):
         self.assertEqual(self.evaluation.related_logentries().count(), 2)
         questionnaire = baker.make(Questionnaire)
-        questionnaire.save()
         contribution = self.evaluation.contributions.get(contributor__isnull=True)
         contribution.questionnaires.add(questionnaire)
-        contribution.save()
         self.assertEqual(self.evaluation.related_logentries().count(), 3)
         self.assertEqual(self.evaluation.related_logentries().order_by("id").last().data['questionnaires']['add'], [questionnaire.id])
 
     def test_none_value_not_included(self):
         contribution = baker.make(Contribution, evaluation=self.evaluation, label="testlabel")
-        contribution.save()
         self.assertIn("label", self.evaluation.related_logentries().order_by("id").last().data)
 
         contribution = baker.make(Contribution, evaluation=self.evaluation, label=None)
-        contribution.save()
         self.assertNotIn("label", self.evaluation.related_logentries().order_by("id").last().data)
