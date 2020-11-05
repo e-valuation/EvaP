370ff42d3fa52ec902eaf6a51b492f4e5f73df40
Jakob Stigloher <pixunil@posteo.eu>
2020-11-05 08:52:03 +0100

    Migrate text-answer-warnings.js to Typescript

---
+++ evap/static/ts/src/text-answer-warnings.ts
@@ -0,0 +1,51 @@
+function normalize(text: string) {
+    return text.toLowerCase().replace(/\s{2,}/g, " ").trim();
+}
+
+function isTextMeaningless(text: string): boolean {
+    return text.length > 0 && ["", "ka", "na"].includes(text.replace(/\W/g, ""));
+}
+
+function doesTextContainTriggerString(text: string, triggerStrings: string[]): boolean {
+    return triggerStrings.some(triggerString => text.includes(triggerString));
+}
+
+function updateTextareaWarning(textarea: HTMLTextAreaElement, textAnswerWarnings: string[][]) {
+    const text = normalize(textarea.value);
+
+    let matchingWarnings = [];
+    if (isTextMeaningless(text)) {
+        matchingWarnings.push("meaningless");
+    }
+    for (const [i, triggerStrings] of textAnswerWarnings.entries()) {
+        if (doesTextContainTriggerString(text, triggerStrings)) {
+            matchingWarnings.push(`trigger-string-${i}`);
+        }
+    }
+
+    const showWarning = matchingWarnings.length > 0;
+    textarea.classList.toggle("border", showWarning);
+    textarea.classList.toggle("border-warning", showWarning);
+    const row = textarea.closest(".row")!;
+    console.debug(matchingWarnings);
+
+    for (const warning of row.querySelectorAll<HTMLElement>("[data-warning]")) {
+        warning.classList.toggle("d-none", !matchingWarnings.includes(warning.dataset.warning!));
+    }
+}
+
+export function initTextAnswerWarnings(textareas: NodeListOf<HTMLTextAreaElement>, textAnswerWarnings: string[][]) {
+    textAnswerWarnings = textAnswerWarnings.map(triggerStrings => triggerStrings.map(normalize));
+
+    textareas.forEach(textarea => {
+        let warningDelayTimer: ReturnType<typeof setTimeout>;
+        textarea.addEventListener("input", () => {
+            clearTimeout(warningDelayTimer);
+            warningDelayTimer = setTimeout(() => updateTextareaWarning(textarea, textAnswerWarnings), 300);
+        });
+        textarea.addEventListener("blur", () => {
+            updateTextareaWarning(textarea, textAnswerWarnings);
+        });
+        updateTextareaWarning(textarea, textAnswerWarnings);
+    });
+}
