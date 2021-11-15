b6c4a58a89c3cdcd028b62f2a48be439a2495b84
Johannes Wolf <johannes-wolf@posteo.de>
2021-11-15 17:51:10 +0100

    add alias command for makemessages and used parameters (#1664)

---
+++ evap/development/management/commands/translate.py
@@ -0,0 +1,11 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    args = ""
+    help = 'Execute "makemessages --locale=de --ignore=node_modules/*"'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Executing "manage.py makemessages --locale=de --ignore=node_modules/*"')
+        call_command("makemessages", "--locale=de", "--ignore=node_modules/*")
