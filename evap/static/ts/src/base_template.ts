80ef8b8155a2ed250481d88db56cf15aa4d55db4
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2021-11-16 13:24:09 +0100

    use indentation of 4 spaces

--- evap/static/ts/src/base_template.ts
+++ evap/static/ts/src/base_template.ts
@@ -2,10 +2,10 @@
 
 // Fix autofocus on bootstrap modals -> Focus the first element with `autofocus` attribute
 document.addEventListener("shown.bs.modal", (e) => {
-  if (!e.target) return;
-  const modalEl = e.target as HTMLElement;
-  const autofocusEl = modalEl.querySelector("[autofocus]") as HTMLElement;
-  if (autofocusEl) {
-    autofocusEl.focus();
-  }
+    if (!e.target) return;
+    const modalEl = e.target as HTMLElement;
+    const autofocusEl = modalEl.querySelector("[autofocus]") as HTMLElement;
+    if (autofocusEl) {
+        autofocusEl.focus();
+    }
 });
