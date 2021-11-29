c000fe5ba1c6b2a652cf1e1a612d17638c049730
fidoriel <49869342+fidoriel@users.noreply.github.com>
2021-11-29 21:17:01 +0100

    Revert "name change"
    This reverts commit d6bee8000bc207d7d9d43e6284416e1cd5a41fd7.

--- deployment/provision_vagrant_vm.sh
+++ deployment/provision_vagrant_vm.sh
@@ -2,7 +2,7 @@
 
 set -x # print executed commands
 
-USER="vagrant"
+USER="evap"
 REPO_FOLDER="/opt/evap"
 ENV_FOLDER="/home/$USER/venv"
 
@@ -11,10 +11,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 # install python stuff
 apt-get -q update
-apt-get -q install -y sudo python3.7 python3.7-dev python3-venv python3.7-venv gettext git wget npm
+apt-get -q install -y python3.7 python3.7-dev python3-venv python3.7-venv gettext
 
 # setup postgres
-printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
 apt-get -q install -y postgresql
 sudo -u postgres createuser --createdb evap
 sudo -u postgres psql -U postgres -d postgres -c "ALTER USER evap WITH PASSWORD 'evap';"
@@ -22,19 +21,17 @@ sudo -u postgres createdb -O evap evap
 
 # setup redis
 apt-get -q install -y redis-server
-sudo sed -i "s/bind .*/bind 127.0.0.1/g" /etc/redis/redis.conf
-sudo service redis-server restart
 
 # install apache
 apt-get -q install -y apache2 apache2-dev
 
 # make user, create home folder, set uid to the same set in the Vagrantfile (required for becoming the synced folder owner), set default shell to bash
-#useradd -m -u 501 -s /bin/bash evap
+useradd -m -u 1042 -s /bin/bash evap
 # allow ssh login
-#cp -r /home/vagrant/.ssh /home/$USER/.ssh
-#chown -R $USER:$USER /home/$USER/.ssh
+cp -r /home/vagrant/.ssh /home/$USER/.ssh
+chown -R $USER:$USER /home/$USER/.ssh
 # allow sudo without password
-#echo "$USER ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/evap
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/evap
 
 # link the mounted evap folder from the home directory
 ln -s /evap $REPO_FOLDER
@@ -59,7 +56,7 @@ a2dissite 000-default.conf
 # see https://github.com/e-valuation/EvaP/issues/626
 # and https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/modwsgi/#if-you-get-a-unicodeencodeerror
 sed -i s,\#.\ /etc/default/locale,.\ /etc/default/locale,g /etc/apache2/envvars
-service apache2 reload
+systemctl reload apache2
 
 cp /etc/skel/.bashrc /home/$USER/
 # auto cd into /$USER on login and activate venv
@@ -74,7 +71,7 @@ cp $REPO_FOLDER/deployment/localsettings.template.py $REPO_FOLDER/evap/localsett
 sed -i -e "s/\${SECRET_KEY}/$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)/" $REPO_FOLDER/evap/localsettings.py
 
 # setup vm auto-completion
-#cp $REPO_FOLDER/deployment/manage_autocompletion.sh /etc/bash_completion.d/
+cp $REPO_FOLDER/deployment/manage_autocompletion.sh /etc/bash_completion.d/
 
 # install libraries for puppeteer
 apt-get -q install -y libasound2 libgconf-2-4 libgbm1 libgtk-3-0 libnss3 libx11-xcb1 libxss1 libxshmfence-dev
@@ -83,7 +80,7 @@ apt-get -q install -y libasound2 libgconf-2-4 libgbm1 libgtk-3-0 libnss3 libx11-
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh --no-verbose --output-document - | sudo -H -u $USER bash
 
 # setup evap
-cd /evap
+cd /$USER
 git submodule update --init
 sudo -H -u $USER bash -c "source /home/$USER/.nvm/nvm.sh; nvm install --no-progress node; npm ci"
 echo "nvm use node" >> /home/$USER/.bashrc
