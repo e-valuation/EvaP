a77dc3b85c1bb2f7831c4581a35b5fe25c2139ee
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-01-31 21:59:07 +0100

    revert bad things

--- Vagrantfile
+++ Vagrantfile
@@ -4,22 +4,13 @@
 Vagrant.require_version ">= 1.8.1"
 
 Vagrant.configure("2") do |config|
-  #config.vm.box = "ubuntu/bionic64"
-  #config.vm.box_version = "= 20210928.0.0 "
-
-  config.vm.provider "docker" do |d|
-    d.build_dir = "."
-    d.remains_running = true
-    d.has_ssh = true
-  end
-
-  #config.ssh.insert_key = false
-
+  config.vm.box = "ubuntu/bionic64"
+  config.vm.box_version = "= 20190621.0.0 "
 
   # port forwarding
   config.vm.network :forwarded_port, guest: 8000, host: 8000 # django server
   config.vm.network :forwarded_port, guest: 80, host: 8001 # apache
-  #config.vm.network :forwarded_port, guest: 6379, host: 6379 # redis. helpful when developing on windows, for which redis is not available
+  config.vm.network :forwarded_port, guest: 6379, host: 6379 # redis. helpful when developing on windows, for which redis is not available
 
   config.vm.provider :virtualbox do |v, _override|
     # disable logfile
@@ -39,10 +30,10 @@ Vagrant.configure("2") do |config|
   # Mounting with uid and gid is necessary as the provision script will create
   # this user, so mounting before does not work with an owner specified by name.
   # Also, provision needs to use vagrant as ssh user (since evap does not exist yet)
-  config.vm.synced_folder ".", "/evap", docker_consistency: "cached"
-  #if ARGV[0] == "ssh" or ARGV[0] == "ssh-config"
-  #  config.ssh.username = 'evap'
-  #end
+  config.vm.synced_folder ".", "/evap", mount_options: ["uid=1042", "gid=1042"]
+  if ARGV[0] == "ssh" or ARGV[0] == "ssh-config"
+    config.ssh.username = 'evap'
+  end
 
   config.vm.provision "shell", path: "deployment/provision_vagrant_vm.sh"
 end
--- deployment/provision_vagrant_vm.sh
+++ deployment/provision_vagrant_vm.sh
@@ -2,7 +2,7 @@
 
 set -x # print executed commands
 
-USER="vagrant"
+USER="evap"
 REPO_FOLDER="/opt/evap"
 ENV_FOLDER="/home/$USER/venv"
 
@@ -11,10 +11,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 # install python stuff
 apt-get -q update
-apt-get -q install -y sudo python3.7 python3.7-dev python3-venv python3.7-venv gettext git wget npm
+apt-get -q install -y python3.7 python3.7-dev python3-venv python3.7-venv gettext
 
 # setup postgres
-printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
 apt-get -q install -y postgresql
 sudo -u postgres createuser --createdb evap
 sudo -u postgres psql -U postgres -d postgres -c "ALTER USER evap WITH PASSWORD 'evap';"
@@ -22,22 +21,23 @@ sudo -u postgres createdb -O evap evap
 
 # setup redis
 apt-get -q install -y redis-server
-sudo sed -i "s/bind .*/bind 127.0.0.1/g" /etc/redis/redis.conf
-sudo service redis-server restart
 
 # install apache
 apt-get -q install -y apache2 apache2-dev
 
 # make user, create home folder, set uid to the same set in the Vagrantfile (required for becoming the synced folder owner), set default shell to bash
-#useradd -m -u 501 -s /bin/bash evap
+useradd -m -u 1042 -s /bin/bash evap
 # allow ssh login
-#cp -r /home/vagrant/.ssh /home/$USER/.ssh
-#chown -R $USER:$USER /home/$USER/.ssh
+cp -r /home/vagrant/.ssh /home/$USER/.ssh
+chown -R $USER:$USER /home/$USER/.ssh
 # allow sudo without password
-#echo "$USER ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/evap
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/evap
 
 # link the mounted evap folder from the home directory
 ln -s /evap $REPO_FOLDER
+
+# install sass
+$REPO_FOLDER/deployment/install_dart_sass.sh
 
 sudo -H -u $USER python3.7 -m venv $ENV_FOLDER
 # venv will use ensurepip to install a new version of pip. We need to update that version.
@@ -50,8 +50,6 @@ a2enmod headers
 cp $REPO_FOLDER/deployment/wsgi.template.conf /etc/apache2/mods-available/wsgi.load
 sed -i -e "s=\${ENV_FOLDER}=$ENV_FOLDER=" /etc/apache2/mods-available/wsgi.load # note this uses '=' as alternate delimiter
 a2enmod wsgi
-a2enmod rewrite
-cp $REPO_FOLDER/deployment/apache.maintenance-template.conf /etc/apache2/sites-available/evap-maintenance.conf
 cp $REPO_FOLDER/deployment/apache.template.conf /etc/apache2/sites-available/evap.conf
 sed -i -e "s=\${ENV_FOLDER}=$ENV_FOLDER=" /etc/apache2/sites-available/evap.conf
 sed -i -e "s=\${REPO_FOLDER}=$REPO_FOLDER=" /etc/apache2/sites-available/evap.conf
@@ -61,9 +59,8 @@ a2dissite 000-default.conf
 # see https://github.com/e-valuation/EvaP/issues/626
 # and https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/modwsgi/#if-you-get-a-unicodeencodeerror
 sed -i s,\#.\ /etc/default/locale,.\ /etc/default/locale,g /etc/apache2/envvars
-service apache2 reload
+systemctl reload apache2
 
-cp /etc/skel/.bashrc /home/$USER/
 # auto cd into /$USER on login and activate venv
 echo "cd $REPO_FOLDER" >> /home/$USER/.bashrc
 echo "source $ENV_FOLDER/bin/activate" >> /home/$USER/.bashrc
@@ -76,18 +73,18 @@ cp $REPO_FOLDER/deployment/localsettings.template.py $REPO_FOLDER/evap/localsett
 sed -i -e "s/\${SECRET_KEY}/$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)/" $REPO_FOLDER/evap/localsettings.py
 
 # setup vm auto-completion
-#cp $REPO_FOLDER/deployment/manage_autocompletion.sh /etc/bash_completion.d/
+cp $REPO_FOLDER/deployment/manage_autocompletion.sh /etc/bash_completion.d/
 
 # install libraries for puppeteer
-apt-get -q install -y libasound2 libgconf-2-4 libgbm1 libgtk-3-0 libnss3 libx11-xcb1 libxss1 libxshmfence-dev
+apt-get -q install -y libasound2 libgconf-2-4 libgbm1 libgtk-3-0 libnss3 libx11-xcb1 libxss1
 
 # install nvm
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh --no-verbose --output-document - | sudo -H -u $USER bash
 
 # setup evap
-cd /evap
+cd /$USER
 git submodule update --init
-sudo -H -u $USER bash -c "source /home/$USER/.nvm/nvm.sh; nvm install --no-progress node; npm ci"
+sudo -H -u $USER bash -c "source /home/$USER/.nvm/nvm.sh; nvm install node; npm ci"
 echo "nvm use node" >> /home/$USER/.bashrc
 sudo -H -u $USER $ENV_FOLDER/bin/python manage.py migrate --noinput
 sudo -H -u $USER $ENV_FOLDER/bin/python manage.py collectstatic --noinput
