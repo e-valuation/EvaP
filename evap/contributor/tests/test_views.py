ebd6a50342f18e9cf2708338b8c9bc5e27bf95fd
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2022-03-21 20:44:38 +0100

    use actual instead of relative count in test

--- evap/contributor/tests/test_views.py
+++ evap/contributor/tests/test_views.py
@@ -263,13 +263,11 @@ class TestContributorEvaluationEditView(WebTest):
         self.evaluation.allow_editors_to_edit = False
         self.evaluation.save()
         page = self.app.get(self.url, user=self.responsible)
-        expected_request_changes_count = page.body.decode().count("Request changes")
-        expected_request_account_count = page.body.decode().count("Request creation of new account")
+        self.assertEqual(page.body.decode().count("Request changes"), 1)
+        self.assertEqual(page.body.decode().count("Request creation of new account"), 1)
 
         self.evaluation.allow_editors_to_edit = True
         self.evaluation.save()
         page = self.app.get(self.url, user=self.responsible)
-        self.assertEqual(page.body.decode().count("Request changes"), expected_request_changes_count - 1)
-        self.assertEqual(
-            page.body.decode().count("Request creation of new account"), expected_request_account_count + 1
-        )
+        self.assertEqual(page.body.decode().count("Request changes"), 0)
+        self.assertEqual(page.body.decode().count("Request creation of new account"), 2)
