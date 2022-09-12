a7af1788b9586c74911b908ee0a214d5b3464d3d
Niklas Mohrin <dev@niklasmohrin.de>
2022-09-12 19:55:06 +0200

    Considered possible security implications associated with Popen module

--- evap/development/management/commands/run.py
+++ evap/development/management/commands/run.py
@@ -1,5 +1,5 @@
 import sys
-from subprocess import Popen
+from subprocess import Popen  # nosec
 
 from django.core.management import execute_from_command_line
 from django.core.management.base import BaseCommand
@@ -11,7 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Executing "manage.py scss" and "manage.py ts compile"')
-        with Popen(["./manage.py", "scss"]), Popen(["./manage.py", "ts", "compile"]):
+        with Popen(["./manage.py", "scss"]), Popen(["./manage.py", "ts", "compile"]):  # nosec
             self.stdout.write('Executing "manage.py runserver 0.0.0.0:8000"')
             sys.argv = ["manage.py", "runserver", "0.0.0.0:8000"]
             execute_from_command_line(sys.argv)
