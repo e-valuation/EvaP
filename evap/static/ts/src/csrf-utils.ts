9d1dd76bda8efee2bb4a03196e4a706cfadf75dc
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-09-12 23:46:19 +0200

    ts

--- evap/static/ts/src/csrf-utils.ts
+++ evap/static/ts/src/csrf-utils.ts
@@ -1,16 +1,6 @@
 // based on: https://docs.djangoproject.com/en/3.1/ref/csrf/#ajax
-function getCookie(name: string): string | null {
-    if (document.cookie !== "") {
-        const cookie = document.cookie
-            .split(";")
-            .map(cookie => cookie.trim())
-            .find(cookie => cookie.substring(0, name.length + 1) === `${name}=`);
-        if (cookie) {
-            return decodeURIComponent(cookie.substring(name.length + 1));
-        }
-    }
-    return null;
-}
+import { getCookie } from "./utils.js";
+
 const csrftoken = getCookie("csrftoken")!;
 export const CSRF_HEADERS = { "X-CSRFToken": csrftoken };
 
--- evap/static/ts/src/utils.ts
+++ evap/static/ts/src/utils.ts
@@ -11,3 +11,20 @@ export function assert(condition: unknown, message: string = "Assertion Failed")
 export const sleep = (ms: number): Promise<number> => {
     return new Promise(resolve => window.setTimeout(resolve, ms));
 };
+
+export function getCookie(name: string): string | null {
+    if (document.cookie !== "") {
+        const cookie = document.cookie
+            .split(";")
+            .map(cookie => cookie.trim())
+            .find(cookie => cookie.substring(0, name.length + 1) === `${name}=`);
+        if (cookie) {
+            return decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    }
+    return null;
+}
+
+export function setCookie(key: string, value: string) {
+    document.cookie = key + "=" + value;
+}
