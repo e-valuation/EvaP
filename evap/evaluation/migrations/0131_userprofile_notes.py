c62423b11a383c9e2a0ef3b2bb72a6329f3700d3
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-09-12 19:42:33 +0200

    update things

--- evap/evaluation/migrations/0131_userprofile_notes.py
+++ evap/evaluation/migrations/0131_userprofile_notes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', '0128_django_40_noop'),
+        ('evaluation', '0130_alter_textanswer_review_decision'),
     ]
 
     operations = [
