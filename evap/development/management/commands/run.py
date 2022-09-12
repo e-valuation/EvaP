1b0a403abd66e4a0f7d409ef7935a0931b80722c
Niklas Mohrin <dev@niklasmohrin.de>
2022-09-12 17:44:54 +0200

    Don't wait for scss and ts compilation in `./manage.py run`
    Instead, the processes are started and left running while the server is
    starting. They exit some time after the server has started.

--- evap/development/management/commands/run.py
+++ evap/development/management/commands/run.py
@@ -1,4 +1,5 @@
 import sys
+from subprocess import Popen
 
 from django.core.management import execute_from_command_line
 from django.core.management.base import BaseCommand
@@ -10,9 +11,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Executing "manage.py scss"')
-        execute_from_command_line(["manage.py", "scss"])
+        Popen(["./manage.py", "scss"])
         self.stdout.write('Executing "manage.py ts compile"')
-        execute_from_command_line(["manage.py", "ts", "compile"])
+        Popen(["./manage.py", "ts", "compile"])
         self.stdout.write('Executing "manage.py runserver 0.0.0.0:8000"')
         sys.argv = ["manage.py", "runserver", "0.0.0.0:8000"]
         execute_from_command_line(sys.argv)
