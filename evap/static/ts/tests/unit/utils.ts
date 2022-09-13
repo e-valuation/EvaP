f27e2e3ef00ff1b65a7878d9cea94e286f1d781e
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-09-13 19:41:17 +0200

    next one

--- evap/static/ts/tests/unit/utils.ts
+++ evap/static/ts/tests/unit/utils.ts
@@ -13,7 +13,7 @@
 // @ts-ignore
 window.$ = require("../../../js/jquery-2.1.3.min");
 
-import { testable } from "src/utils.js";
+import { testable } from "src/utils";
 
 const { getCookie } = testable;
 
