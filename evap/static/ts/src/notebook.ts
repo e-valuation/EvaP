dd4cc92b1de6f6cbb7a6b39acf2e035a8ba470e6
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-10-24 22:29:15 +0200

    Revert "null"
    This reverts commit 25ade842b25ccbf980612009acee3d28ac962352.

--- evap/static/ts/src/notebook.ts
+++ evap/static/ts/src/notebook.ts
@@ -1,34 +1,31 @@
 declare const bootstrap: typeof import("bootstrap");
-import { getCookie, setCookie, selectOrError } from "./utils.js";
+import { getCookie, setCookie } from "./utils.js";
 
 document.getElementById("123");
 
-var notebook = selectOrError("notebook");
-var evapContent = selectOrError("evapContent");
-
 if (getCookie("evap_notebook_open") == "true") {
-    new bootstrap.Collapse(notebook);
-    new bootstrap.Collapse(selectOrError("#notebookButton"));
+    new bootstrap.Collapse(document.querySelector("#notebook")!);
+    new bootstrap.Collapse(document.querySelector("#notebookButton")!);
     onShowNotebook();
 }
 
 // make evap go away for notebook
-notebook.addEventListener("show.bs.collapse", function () {
+document.querySelector("#notebook")!.addEventListener("show.bs.collapse", function () {
     onShowNotebook();
     setCookie("evap_notebook_open", "true");
 });
 
-notebook.addEventListener("hidden.bs.collapse", function () {
+document.querySelector("#notebook")!.addEventListener("hidden.bs.collapse", function () {
     onHideNotebook();
     setCookie("evap_notebook_open", "false");
 });
 
 export function onShowNotebook(): void {
-    evapContent.classList.add("notebook-margin");
-    notebook.classList.add("notebook-container");
+    document.getElementById("evapContent")!.classList.add("notebook-margin");
+    document.getElementById("notebook")!.classList.add("notebook-container");
 }
 
 export function onHideNotebook(): void {
-    evapContent.classList.remove("notebook-margin");
-    notebook.classList.remove("notebook-container");
+    document.getElementById("evapContent")!.classList.remove("notebook-margin");
+    document.getElementById("notebook")!.classList.remove("notebook-container");
 }
