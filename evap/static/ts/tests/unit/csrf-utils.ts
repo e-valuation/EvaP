f995a46b507cdf954fba30ce90cfa342412ef22f
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-10-24 20:19:06 +0200

    move tests

---
+++ evap/static/ts/tests/unit/csrf-utils.ts
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ Object.defineProperty(document, "cookie", {
+    get: () =>
+        `foo=${encodeURIComponent("F00")}; ` +
+        `csrftoken=${encodeURIComponent("token")}; ` +
+        `bar=${encodeURIComponent("+{)")}; ` +
+        `baz=${encodeURIComponent("+{`")}`,
+});
+
+// @ts-ignore
+window.$ = require("../../../js/jquery-2.1.3.min");
+
+import { testable } from "src/csrf-utils";
+
+const { isMethodCsrfSafe } = testable;
+
+test.each(["GET", "HEAD", "OPTIONS", "TRACE"])("method %s is considered safe", method => {
+    expect(isMethodCsrfSafe(method)).toBe(true);
+});
+
+test.each(["POST", "PUT", "DELETE"])("method %s is considered unsafe", method => {
+    expect(isMethodCsrfSafe(method)).toBe(false);
+});
+
+test("send csrf token in request", () => {
+    const mock = {
+        open: jest.fn(),
+        send: jest.fn(),
+        setRequestHeader: jest.fn(),
+    };
+    window.XMLHttpRequest = jest.fn(() => mock) as unknown as typeof window.XMLHttpRequest;
+
+    $.post("/receiver");
+
+    expect(mock.setRequestHeader).toBeCalledWith("X-CSRFToken", "token");
+});
--- evap/static/ts/tests/unit/utils.ts
+++ evap/static/ts/tests/unit/utils.ts
@@ -15,7 +15,7 @@ window.$ = require("../../../js/jquery-2.1.3.min");
 
 import { testable } from "src/utils";
 
-const { getCookie, isMethodCsrfSafe } = testable;
+const { getCookie } = testable;
 
 test("parse cookie", () => {
     expect(getCookie("foo")).toBe("F00");
@@ -23,25 +23,4 @@ test("parse cookie", () => {
     expect(getCookie("baz")).toBe("+{`");
     expect(getCookie("csrftoken")).toBe("token");
     expect(getCookie("qux")).toBe(null);
-});
-
-test.each(["GET", "HEAD", "OPTIONS", "TRACE"])("method %s is considered safe", method => {
-    expect(isMethodCsrfSafe(method)).toBe(true);
-});
-
-test.each(["POST", "PUT", "DELETE"])("method %s is considered unsafe", method => {
-    expect(isMethodCsrfSafe(method)).toBe(false);
-});
-
-test("send csrf token in request", () => {
-    const mock = {
-        open: jest.fn(),
-        send: jest.fn(),
-        setRequestHeader: jest.fn(),
-    };
-    window.XMLHttpRequest = jest.fn(() => mock) as unknown as typeof window.XMLHttpRequest;
-
-    $.post("/receiver");
-
-    expect(mock.setRequestHeader).toBeCalledWith("X-CSRFToken", "token");
 });
