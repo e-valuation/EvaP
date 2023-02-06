ae85a56aa2163a16ce0cc1da83ecb6f81e1dd35e
Kakadus <58814480+Kakadus@users.noreply.github.com>
2023-02-06 21:29:53 +0100

    scss and typescript!!

--- evap/static/ts/src/infobox.ts
+++ evap/static/ts/src/infobox.ts
@@ -1,8 +1,4 @@
-declare const bootstrap: typeof import("bootstrap");
-
 import { selectOrError } from "./utils.js";
-
-const OPEN_CLOSE_TIMEOUT = 2000;
 
 export class InfoboxLogic {
     private readonly infobox: HTMLDivElement;
@@ -18,21 +14,17 @@ export class InfoboxLogic {
     public attach = (): void => {
         // close the infobox and save state
         this.closeButton.addEventListener("click", async event => {
-            this.infobox.classList.add("closing");
-            setTimeout(() => {
-                this.infobox.classList.replace("closing", "closed");
-            }, OPEN_CLOSE_TIMEOUT);
-            event.stopPropagation(); // prevent immediate reopening
-            localStorage[this.storageKey] = "hide";
+            if (!this.infobox.className.includes("closed")) {
+                this.infobox.classList.add("closed");
+                event.stopPropagation(); // prevent immediate reopening: close button is part of the header, which is used as the open button
+                localStorage[this.storageKey] = "hide";
+            }
         });
 
         // open the infobox and save state
         this.infobox.addEventListener("click", async _ => {
             if (this.infobox.className.includes("closed")) {
-                this.infobox.classList.replace("closed", "opening");
-                setTimeout(() => {
-                    this.infobox.classList.remove("opening");
-                }, OPEN_CLOSE_TIMEOUT);
+                this.infobox.classList.remove("closed");
                 localStorage[this.storageKey] = "show";
             }
         });
