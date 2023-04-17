d58d2d6e775148de82b4f202fd9f00ca02456d2b
fidoriel <49869342+fidoriel@users.noreply.github.com>
2023-04-17 23:43:09 +0200

    cooldown; 204 check and error msg

--- evap/static/ts/src/notebook.ts
+++ evap/static/ts/src/notebook.ts
@@ -10,6 +10,39 @@ if (getCookie("evap_notebook_open") == "true") {
     onShowNotebook();
 }
 
+assertDefinedUnwrap(document.getElementById("notebook-save-button")).addEventListener("click", function (event): void {
+    event.preventDefault();
+    const form = assertDefinedUnwrap(document.getElementById("notebook-form")) as HTMLFormElement;
+    const data = new FormData(form);
+
+    var target = assertDefinedUnwrap(event.target) as HTMLButtonElement;
+    let default_label = assertDefinedUnwrap(target.getAttribute("value"));
+    let error_label = assertDefinedUnwrap(target.getAttribute("value"));
+    target.disabled = true;
+
+    let form_json = JSON.stringify(Object.fromEntries(data.entries()));
+
+    fetch(form.action, {
+        method: "POST",
+        body: form_json,
+        headers: {
+            "X-CSRFToken": getCookie("csrftoken")!,
+            "Content-Type": "application/json",
+        },
+    }) // check for 204
+        .then((response) => {
+            if (response.status == 204) {
+                target.setAttribute("value", assertDefinedUnwrap(target.getAttribute("data-label-cooldown")));
+                setTimeout(function (): void {
+                    target.disabled = false;
+                    target.setAttribute("value", default_label);
+                }, 2000);
+            } else {
+                alert(error_label);
+            }
+        })
+    });
+
 // https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/1535
 assertDefinedUnwrap(document.getElementById("notebook")).addEventListener("show.bs.collapse", function (): void {
     setCookie(NOTEBOOK_COOKIE_NAME, "true");
