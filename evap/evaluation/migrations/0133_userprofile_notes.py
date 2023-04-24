e7b3cbf2fda6df887efed9145722fe1bf18d5fe9
fidoriel <49869342+fidoriel@users.noreply.github.com>
2023-04-24 22:08:36 +0200

    fix migration

--- evap/evaluation/migrations/0133_userprofile_notes.py
+++ evap/evaluation/migrations/0133_userprofile_notes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', '0131_userprofile_ordering'),
+        ('evaluation', '0132_textanswer_is_flagged'),
     ]
 
     operations = [
