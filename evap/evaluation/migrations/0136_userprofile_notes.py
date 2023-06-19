9283699843ed59e962ce7f8d3561f8e3f4978630
fidoriel <49869342+fidoriel@users.noreply.github.com>
2023-06-19 20:08:35 +0200

    fix migration

--- evap/evaluation/migrations/0136_userprofile_notes.py
+++ evap/evaluation/migrations/0136_userprofile_notes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', "0134_userprofile_display_name"),
+        ('evaluation', "0135_consistent_no_data_representation_on_text_fields"),
     ]
 
     operations = [
