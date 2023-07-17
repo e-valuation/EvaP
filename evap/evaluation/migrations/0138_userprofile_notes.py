2c587c8c987e350a6ff70638901165910b97c5ba
fidoriel <49869342+fidoriel@users.noreply.github.com>
2023-07-17 19:56:38 +0200

    merge main

--- evap/evaluation/migrations/0138_userprofile_notes.py
+++ evap/evaluation/migrations/0138_userprofile_notes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('evaluation', "0136_alter_userprofile_first_name_chosen_and_more"),
+        ('evaluation', "0137_use_more_database_constraints"),
     ]
 
     operations = [
