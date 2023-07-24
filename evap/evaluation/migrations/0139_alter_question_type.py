650295dd12cc37490af678b2ab5dcd0eebc5e6d5
FSadrieh <frederic.sadrieh@student.hpi.de>
2023-07-24 20:13:54 +0200

    Rebased

--- evap/evaluation/migrations/0139_alter_question_type.py
+++ evap/evaluation/migrations/0139_alter_question_type.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("evaluation", "0137_use_more_database_constraints"),
+        ("evaluation", "0138_votetimestamp"),
     ]
 
     operations = [
