af3397481f6c44c1088149b860dc38d0a41f769f
Richard Ebeling <dev@richardebeling.de>
2023-10-22 20:21:27 +0200

    Fix type errors that django-stubs==4.2.5 introduced

--- evap/evaluation/models.py
+++ evap/evaluation/models.py
@@ -1681,11 +1681,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     # delegates of the user, which can also manage their evaluations
     delegates = models.ManyToManyField(
-        "UserProfile", verbose_name=_("Delegates"), related_name="represented_users", blank=True
+        "evaluation.UserProfile", verbose_name=_("Delegates"), related_name="represented_users", blank=True
     )
 
     # users to which all emails should be sent in cc without giving them delegate rights
-    cc_users = models.ManyToManyField("UserProfile", verbose_name=_("CC Users"), related_name="ccing_users", blank=True)
+    cc_users = models.ManyToManyField(
+        "evaluation.UserProfile", verbose_name=_("CC Users"), related_name="ccing_users", blank=True
+    )
 
     # flag for proxy users which represent a group of users
     is_proxy_user = models.BooleanField(default=False, verbose_name=_("Proxy user"))
@@ -1725,7 +1727,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _("users")
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS: list[str] = []
+    REQUIRED_FIELDS = []
 
     objects = UserProfileManager()
 
