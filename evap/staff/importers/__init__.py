ee949d0bd9afee750c42c5000dd44eed57def463
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2023-10-23 22:08:47 +0200

    add __all__

--- evap/staff/importers/__init__.py
+++ evap/staff/importers/__init__.py
@@ -2,3 +2,12 @@ from .base import ImporterLog, ImporterLogEntry
 from .enrollment import import_enrollments
 from .person import import_persons_from_evaluation, import_persons_from_file
 from .user import import_users
+
+__all__ = [
+    "ImporterLog",
+    "ImporterLogEntry",
+    "import_enrollments",
+    "import_persons_from_evaluation",
+    "import_persons_from_file",
+    "import_users",
+]
