5653954435a0e1499c2b30c52e5b7e68163a4c20
ybrnr <yannik.brunner@student.hpi.uni-potsdam.de>
2023-10-30 18:11:02 +0100

    try to fix 2002

--- evap/results/exporters.py
+++ evap/results/exporters.py
@@ -160,18 +160,22 @@ class ResultsExporter(ExcelExporter):
         return evaluations_with_results, used_questionnaires, course_results_exist
 
     def write_headings_and_evaluation_info(
-        self, evaluations_with_results, semesters, contributor, degrees, course_types
+        self, evaluations_with_results, semesters, contributor, degrees, course_types, verbose_heading
     ):
-        export_name = "Evaluation"
+        export_name = "Evaluation "
         if contributor:
             export_name += f"\n{contributor.full_name}"
         elif len(semesters) == 1:
             export_name += f"\n{semesters[0].name}"
-        degree_names = [degree.name for degree in Degree.objects.filter(pk__in=degrees)]
-        course_type_names = [course_type.name for course_type in CourseType.objects.filter(pk__in=course_types)]
-        self.write_cell(
-            _("{}\n\n{}\n\n{}").format(export_name, ", ".join(degree_names), ", ".join(course_type_names)), "headline"
-        )
+        if verbose_heading:
+            degree_names = [degree.name for degree in Degree.objects.filter(pk__in=degrees)]
+            course_type_names = [course_type.name for course_type in CourseType.objects.filter(pk__in=course_types)]
+            self.write_cell(
+                _("{}\n{}\n{}").format(export_name, ", ".join(degree_names), ", ".join(course_type_names)),
+                "headline",
+            )
+        else:
+            self.write_cell(_("{}\n\n".format(export_name)))
 
         for evaluation, __ in evaluations_with_results:
             title = evaluation.full_name
@@ -285,7 +289,13 @@ class ResultsExporter(ExcelExporter):
 
     # pylint: disable=arguments-differ
     def export_impl(
-        self, semesters, selection_list, include_not_enough_voters=False, include_unpublished=False, contributor=None
+        self,
+        semesters,
+        selection_list,
+        include_not_enough_voters=False,
+        include_unpublished=False,
+        contributor=None,
+        verbose_heading=True,
     ):
         # We want to throw early here, since workbook.save() will throw an IndexError otherwise.
         assert len(selection_list) > 0
@@ -309,7 +319,7 @@ class ResultsExporter(ExcelExporter):
             )
 
             self.write_headings_and_evaluation_info(
-                evaluations_with_results, semesters, contributor, degrees, course_types
+                evaluations_with_results, semesters, contributor, degrees, course_types, verbose_heading
             )
 
             for questionnaire in used_questionnaires:
