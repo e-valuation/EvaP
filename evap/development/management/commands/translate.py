26b9bbac5cd8fc19554afd624827b97e2954cd89
Jonathan Weth <git@jonathanweth.de>
2023-11-20 21:44:39 +0100

    Fix generation of JS translations by dropping old locale folder

--- evap/development/management/commands/translate.py
+++ evap/development/management/commands/translate.py
@@ -9,6 +9,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Executing "manage.py makemessages --locale=de --ignore=node_modules/*"')
         call_command("makemessages", "--locale=de", "--ignore=node_modules/*")
+        self.stdout.write(
+            'Executing ""makemessages --domain=djangojs --extension=js,ts "'
+            '"--locale=de --ignore=node_modules/* --ignore=evap/static/js/*.min.js",'
+        )
         call_command(
             "makemessages",
             "--domain=djangojs",
