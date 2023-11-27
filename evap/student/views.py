9605f8adac55e075438f9c8a785f5c3aa8027656
FSadrieh <frederic.sadrieh@student.hpi.de>
2023-11-27 18:52:33 +0100

    Made code nicer

--- evap/student/views.py
+++ evap/student/views.py
@@ -162,14 +162,14 @@ def render_vote_page(request, evaluation, preview, for_rendering_in_modal=False)
         evaluation_form_group_bottom = []
 
     contributor_errors_exist = any(form.errors for form_group in form_groups.values() for form in form_group)
-    general_errors_exist = any(
+    errors_exist = contributor_errors_exist or any(
         any(form.errors for form in form_group)
         for form_group in [evaluation_form_group_top, evaluation_form_group_bottom]
     )
 
     template_data = {
         "contributor_errors_exist": contributor_errors_exist,
-        "errors_exist": contributor_errors_exist or general_errors_exist,
+        "errors_exist": errors_exist,
         "evaluation_form_group_top": evaluation_form_group_top,
         "evaluation_form_group_bottom": evaluation_form_group_bottom,
         "contributor_form_groups": contributor_form_groups,
