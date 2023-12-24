6aecca3f9de01fb52f3e29571bea2371b6f4dc9c
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2023-12-24 09:53:09 +0100

    Fix spelling and grammar
    Co-authored-by: Richard Ebeling <dev@richardebeling.de>

--- evap/settings.py
+++ evap/settings.py
@@ -407,7 +407,7 @@ OIDC_OP_JWKS_ENDPOINT = "https://example.com/certs"
 # Create a localsettings.py if you want to locally override settings
 # and don't want the changes to appear in 'git status'.
 try:
-    # localsettings file may exist (vagrant) or do not (GitHub)
+    # localsettings file may (vagrant) or may not (CI run) exist
 
     # the import can overwrite locals with a slightly different type (e.g. DATABASES), which is fine.
     from evap.localsettings import *  # type: ignore  # noqa: F403,PGH003
