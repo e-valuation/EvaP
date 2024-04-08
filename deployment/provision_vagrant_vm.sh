86eabb86c157a7cfd2cf49c1603cbea9b0298a93
Jonathan Weth <git@jonathanweth.de>
2024-04-08 19:58:16 +0200

    Use current Firefox version

--- deployment/provision_vagrant_vm.sh
+++ deployment/provision_vagrant_vm.sh
@@ -94,23 +94,18 @@ cp $REPO_FOLDER/deployment/manage_autocompletion.sh /etc/bash_completion.d/
 apt-get -q install -y chromium-browser
 
 # install firefox and geckodriver
-cat <<EOT >> /etc/apt/preferences.d/mozillateam
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
 Package: *
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 100
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla 
+apt update
+apt -q install -y firefox
 
-Package: firefox*
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 1001
-
-Package: firefox*
-Pin: release o=Ubuntu
-Pin-Priority: -1
-EOT
-add-apt-repository -y ppa:mozillateam/ppa
-apt-get -q install -y firefox
-
-wget https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux64.tar.gz -O geckodriver.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux64.tar.gz -O geckodriver.tar.gz
 tar xzf geckodriver.tar.gz -C /usr/local/bin/
 chmod +x /usr/local/bin/geckodriver
 
