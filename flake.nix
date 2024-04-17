e1062c3dd91fa01f73ef1b98c1d3e6903139b1c9
Niklas Mohrin <dev@niklasmohrin.de>
2024-04-17 17:25:51 +0200

    Possible setup with services
    Use
        nix run .#services
    to start postgres and redis and in another terminal run
        nix develop
        ./manage.py migrate
        ./manage.py run
    
    This doesn't compile scss yet, but the basic website shows up.

--- flake.nix
+++ flake.nix
@@ -2,37 +2,74 @@
   description = "EvaP";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
-        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
-      in
-      {
-        packages = {
-          evap = poetry2nix.mkPoetryApplication {
-            projectDir = self;
-            preferWheels = true;
-            overrides = poetry2nix.overrides.withDefaults (final: prev: {
-              # https://github.com/nix-community/poetry2nix/issues/1499
-              django-stubs-ext = prev.django-stubs-ext.override { preferWheel = false; };
-            });
+  outputs = { self, nixpkgs, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { config, system, ... }:
+        let
+          # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (pkgs) lib;
+          poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        in
+        {
+          packages = {
+            evap = poetry2nix.mkPoetryApplication {
+              projectDir = self;
+              preferWheels = true;
+              overrides = poetry2nix.overrides.withDefaults (final: prev: {
+                # https://github.com/nix-community/poetry2nix/issues/1499
+                django-stubs-ext = prev.django-stubs-ext.override { preferWheel = false; };
+              });
+            };
           };
-          default = self.packages.${system}.evap;
-        };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.evap ];
-          packages = [ pkgs.poetry ];
+          # Start with `nix run .#services`
+          process-compose."services" = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+            ];
+
+            services = {
+              redis."r1".enable = true;
+              postgres."pg1" = {
+                enable = true;
+                # initialDatabases = [
+                #   { name = "evap"; }
+                # ];
+                initialScript.before = ''
+                    CREATE USER evap;
+                    ALTER USER evap WITH PASSWORD 'evap';
+                    CREATE DATABASE evap OWNER evap;
+                '';
+              };
+            };
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              inputsFrom = [ self.packages.${system}.evap ];
+              packages = with pkgs; [
+                poetry
+              ];
+            };
+          };
         };
-      });
+    };
 }
