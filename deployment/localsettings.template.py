734317cdd64248c2e809fb33bdb22c630da7de66
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-08-01 22:06:09 +0200

    remove outdated db comment (#2254)

--- deployment/localsettings.template.py
+++ deployment/localsettings.template.py
@@ -4,12 +4,12 @@ from django.utils.safestring import mark_safe
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',   # postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'evap',                         # Or path to database file if using sqlite3.
-        'USER': 'evap',                         # Not used with sqlite3.
-        'PASSWORD': 'evap',                     # Not used with sqlite3.
-        'HOST': '127.0.0.1',                    # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'evap',
+        'USER': 'evap',
+        'PASSWORD': 'evap',
+        'HOST': '127.0.0.1',                    # Set to empty string for localhost.
+        'PORT': '',                             # Set to empty string for default.
         'CONN_MAX_AGE': 600,
     }
 }
