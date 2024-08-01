fb04bcc05e04d964a3093135579497a0d6a92d3e
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-08-01 15:59:37 +0200

    remove spaces (hopefully all)

--- deployment/localsettings.template.py
+++ deployment/localsettings.template.py
@@ -1,9 +1,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'evap',                       
-        'USER': 'evap',                     
-        'PASSWORD': 'evap',             
+        'NAME': 'evap',
+        'USER': 'evap',
+        'PASSWORD': 'evap',
         'HOST': '127.0.0.1',                    # Set to empty string for localhost.
         'PORT': '',                             # Set to empty string for default.
         'CONN_MAX_AGE': 600,
