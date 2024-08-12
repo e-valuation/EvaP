bc160bc3861c351265c862458a0f0207afc428c1
Jonathan Weth <git@jonathanweth.de>
2024-08-12 18:28:11 +0200

    Introduce step for RewardPointRedemptionEvent

--- evap/rewards/forms.py
+++ evap/rewards/forms.py
@@ -6,7 +6,7 @@ from evap.rewards.models import RewardPointRedemptionEvent
 class RewardPointRedemptionEventForm(forms.ModelForm):
     class Meta:
         model = RewardPointRedemptionEvent
-        fields = ("name", "date", "redeem_end_date")
+        fields = ("name", "date", "redeem_end_date", "step")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
