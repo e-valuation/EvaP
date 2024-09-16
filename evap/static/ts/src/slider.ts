cba58b47d812fb5aafcc13811abc09eef405aebe
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-09-16 22:59:24 +0200

    lint ts

--- evap/static/ts/src/slider.ts
+++ evap/static/ts/src/slider.ts
@@ -6,39 +6,19 @@ export interface Range {
 }
 
 export class RangeSlider {
-    set range(range: Range) {
-        this.low = range.low;
-        this.high = range.high;
-        this.updateRange();
-    }
-
-    get range(): Range {
-        return { low: this.low, high: this.high };
-    }
-
-    private readonly rangeSlider: HTMLDivElement;
     public readonly lowSlider: HTMLInputElement;
     public readonly highSlider: HTMLInputElement;
+
+    private readonly rangeSlider: HTMLDivElement;
     private readonly maxLabel: HTMLSpanElement;
     private readonly minLabel: HTMLSpanElement;
     private readonly rangeLabel: HTMLSpanElement;
     private readonly min: number = 0;
-    private max: number = 0;
-    private low: number = 0;
-    private high: number = 0;
+    private max = 0;
+    private low = 0;
+    private high = 0;
 
-    public onRangeChange: () => void = () => {};
-
-    public includeValues(values: number[]) {
-        const max = Math.max(...values);
-        if (max > this.max) {
-            this.max = max;
-            this.updateLimits();
-            this.reset();
-        }
-    }
-
-    constructor(sliderId: string) {
+    public constructor(sliderId: string) {
         this.rangeSlider = selectOrError<HTMLDivElement>("#" + sliderId);
         this.lowSlider = selectOrError<HTMLInputElement>("[name=low]", this.rangeSlider);
         this.highSlider = selectOrError<HTMLInputElement>("[name=high]", this.rangeSlider);
@@ -46,7 +26,7 @@ export class RangeSlider {
         this.maxLabel = selectOrError<HTMLSpanElement>(".text-end", this.rangeSlider);
         this.rangeLabel = selectOrError<HTMLSpanElement>(".range-values", this.rangeSlider);
 
-        const updateFromSlider = () => {
+        const updateFromSlider = (): void => {
             this.low = parseFloat(this.lowSlider.value);
             this.high = parseFloat(this.highSlider.value);
             this.updateRange();
@@ -56,7 +36,35 @@ export class RangeSlider {
         this.highSlider.addEventListener("input", updateFromSlider);
     }
 
-    private updateRange() {
+    public get range(): Range {
+        return { low: this.low, high: this.high };
+    }
+
+    public set range(range: Range) {
+        this.low = range.low;
+        this.high = range.high;
+        this.updateRange();
+    }
+
+    public onRangeChange(): void {}
+
+    public includeValues(values: number[]): void {
+        const max = Math.max(...values);
+        if (max > this.max) {
+            this.max = max;
+            this.updateLimits();
+            this.reset();
+        }
+    }
+
+    public reset(): void {
+        this.range = { low: this.min, high: this.max };
+        this.lowSlider.value = this.low.toString();
+        this.highSlider.value = this.high.toString();
+        this.updateRange();
+    }
+
+    private updateRange(): void {
         if (this.low > this.high) {
             const tmp = this.low;
             this.low = this.high;
@@ -66,19 +74,12 @@ export class RangeSlider {
         this.onRangeChange();
     }
 
-    private updateLimits() {
+    private updateLimits(): void {
         this.lowSlider.min = this.min.toString();
         this.lowSlider.max = this.max.toString();
         this.highSlider.min = this.min.toString();
         this.highSlider.max = this.max.toString();
         this.minLabel.innerText = this.min.toString();
         this.maxLabel.innerText = this.max.toString();
-    }
-
-    public reset() {
-        this.range = { low: this.min, high: this.max };
-        this.lowSlider.value = this.low.toString();
-        this.highSlider.value = this.high.toString();
-        this.updateRange();
     }
 }
