fcd83e4dcdfbe18879882d0cd3737022c124a08a
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-09-23 19:54:51 +0200

    add eslint tseslint and configure it so that it passes with warnings

---
+++ evap/static/ts/eslint.config.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        ignores: ["rendered/", "eslint.config.js"],
+    },
+    {
+        languageOptions: {
+            ecmaVersion: 5,
+            parserOptions: {
+                project: ["tsconfig.json", "tsconfig.compile.json"],
+            },
+            sourceType: "module",
+        },
+    },
+    // ignore @typescript-eslint/no-misused-promises
+    {
+        rules: {
+            "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }],
+            // not fixed in this PR
+            "@typescript-eslint/no-misused-promises": "off",
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/no-unsafe-call": "off",
+            "@typescript-eslint/no-unsafe-member-access": "off",
+            "@typescript-eslint/no-unsafe-argument": "off",
+            "@typescript-eslint/no-unsafe-assignment": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/no-unnecessary-type-parameters": "off",
+            // fixed in this PR
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/no-confusing-void-expression": "warn",
+            "@typescript-eslint/await-thenable": "warn",
+            "@typescript-eslint/no-floating-promises": "warn",
+            "@typescript-eslint/no-deprecated": "warn",
+            "@typescript-eslint/consistent-generic-constructors": "warn",
+            "@typescript-eslint/no-unnecessary-type-arguments": "warn",
+            "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+            "@typescript-eslint/no-inferrable-types": "warn",
+            "prefer-const": "warn",
+            "@typescript-eslint/no-unnecessary-condition": "warn",
+            "@typescript-eslint/non-nullable-type-assertion-style": "warn",
+            "@typescript-eslint/array-type": "warn",
+            "@typescript-eslint/no-unsafe-enum-comparison": "warn",
+            "@typescript-eslint/use-unknown-in-catch-callback-variable": "warn",
+            "@typescript-eslint/no-unsafe-return": "warn",
+            "@typescript-eslint/no-redundant-type-constituents": "warn",
+            "@typescript-eslint/prefer-optional-chain": "warn",
+        },
+    },
+);
