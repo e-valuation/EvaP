5b82cc554a41735e47ee01831cbf722ad998c5cc
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-10-07 20:34:30 +0200

    fixup! set unusable passwords by default

--- evap/evaluation/migrations/0147_unusable_password_default.py
+++ evap/evaluation/migrations/0147_unusable_password_default.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("evaluation", "0145_rename_degree_to_program"),
+        ("evaluation", "0146_grade_reminder_template"),
     ]
 
     operations = [
