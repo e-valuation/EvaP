1b0e1ca723e72453afe3579c0a9daea4884810e9
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-10-07 20:24:22 +0200

    remove unused methods from abstract base user

--- evap/evaluation/models.py
+++ evap/evaluation/models.py
@@ -1680,64 +1680,40 @@ class UserProfileManager(BaseUserManager):
 class EvapBaseUser(models.Model):
     """This is strongly related to the django.contrib.auth.base_user.AbstractBaseUser model, but does not define natural_key."""
 
-    password = models.CharField(_("password"), max_length=128)
+    USERNAME_FIELD: str
+
     # set unusable password by default: We don't want password resets.
     password = models.CharField(_("password"), max_length=128, default=partial(make_password, None))
     last_login = models.DateTimeField(_("last login"), blank=True, null=True)
-
-    # Stores the raw password if set_password() is called so that it can
-    # be passed to password_changed() after the model is saved.
-    _password = None
 
     class Meta:
         abstract = True
 
     def get_username(self):
-        """Return the username for this User."""
+        # required for django-webtest. See https://github.com/django-webtest/django-webtest/issues/134.
         return getattr(self, self.USERNAME_FIELD)
 
     @property
     def is_anonymous(self):
-        """
-        Always return False. This is a way of comparing User objects to
-        anonymous users.
-        """
+        # django.contrib.auth.checks requires this to be MethodType
         return False
 
     @property
     def is_authenticated(self):
-        """
-        Always return True. This is a way to tell if the user has been
-        authenticated in templates.
-        """
         return True
 
     def set_password(self, raw_password):
         self.password = make_password(raw_password)
-        self._password = raw_password
 
     def check_password(self, raw_password):
-        """
-        Return a boolean of whether the raw_password was correct. Handles
-        hashing formats behind the scenes.
-        """
-
         def setter(raw_password):
             self.set_password(raw_password)
             # Password hash upgrades shouldn't be considered password changes.
-            self._password = None
             self.save(update_fields=["password"])
 
         return check_password(raw_password, self.password, setter)
 
-    def set_unusable_password(self):
-        # Set a value that will never be a valid hash
-        self.password = make_password(None)
-
     def has_usable_password(self):
-        """
-        Return False if set_unusable_password() has been called for this user.
-        """
         return is_password_usable(self.password)
 
 
