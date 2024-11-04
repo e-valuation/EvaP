67a1b2d96d1de38c382c5d3454357c59eee7543f
Niklas Mohrin <dev@niklasmohrin.de>
2024-11-04 19:12:05 +0100

    Make EvaP pip-installable and publish to PyPI
    Publish on main PyPI
    
    Remove unneeded code to add `evap` to PYTHONPATH
    
    Run `manage.py` on `python -m evap`
    
    fix manage.py
    
    possibly package with hatch?

--- manage.py
+++ manage.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
-import os
-import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evap.settings")
+from evap.__main__ import main
 
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+main()
