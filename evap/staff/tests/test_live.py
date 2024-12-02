25534534d96ce905d4bfa86a6ba2e8270337b064
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2024-12-02 17:28:05 +0100

    remove impossible clipboard test

--- evap/staff/tests/test_live.py
+++ evap/staff/tests/test_live.py
@@ -11,25 +11,6 @@ from evap.evaluation.tests.tools import LiveServerTest, make_manager
 
 
 class StaffLiveTests(LiveServerTest):
-    def test_copy_header(self):
-        self._default_login()
-
-        self._enter_staff_mode()
-
-        self.selenium.get(self.live_server_url + reverse("staff:user_import"))
-
-        WebDriverWait(self.selenium, 10).until(
-            expected_conditions.visibility_of_element_located((By.CLASS_NAME, "btn-link"))
-        )
-
-        # Patch clipboard functions to test functionality
-        self.selenium.execute_script("navigator.clipboardExecuted = false;")
-        self.selenium.execute_script("navigator.clipboard.writeText = (c) => navigator.clipboardExecuted = c;")
-
-        self.selenium.find_element(By.CLASS_NAME, "btn-link").click()
-
-        copied_text = self.selenium.execute_script("return navigator.clipboardExecuted;")
-        self.assertEqual(copied_text, "Title\tFirst name\tLast name\tEmail")
 
     def test_changes_form_data(self):
         manager = make_manager()
