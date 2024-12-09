92b7761c7de880067b6bcd958b84c4fe52891c23
Niklas Mohrin <dev@niklasmohrin.de>
2024-12-09 22:11:49 +0100

    create data dir?

--- evap/__main__.py
+++ evap/__main__.py
@@ -3,11 +3,14 @@
 import os
 import sys
 
+from django.conf import settings
 from django.core.management import execute_from_command_line
 
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evap.settings")
+    if getattr(settings, "DATADIR", None) is not None:
+        settings.DATADIR.mkdir(exist_ok=True)
     execute_from_command_line(sys.argv)
 
 
