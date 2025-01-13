93879f4bd147a5797a99a1a6d4f4eda651537c0c
Niklas Mohrin <dev@niklasmohrin.de>
2025-01-13 20:33:33 +0100

    Delete remaining files from deployment; moved to evap-deployment repo
    remove maintenance mode files

--- deployment/productionsettings.template.py
+++
@@ -1,3 +0,0 @@
-from evap.settings import *  # noqa: F403
-
-SECRET_KEY = "my-secret-key"
--- deployment/wsgi.template.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "productionsettings")
-
-from evap.wsgi import application  # noqa: F401
