e5a8789ec8fc93af610ab9f46f39686f315e7760
Jonas Dittrich <58814480+Kakadus@users.noreply.github.com>
2025-01-20 21:55:43 +0100

    use async clipboard api

--- evap/static/ts/src/copy-to-clipboard.ts
+++ evap/static/ts/src/copy-to-clipboard.ts
@@ -1,19 +1,3 @@
-function copyToClipboard(text: string) {
-    const selection = document.getSelection()!;
-    const el = document.createElement("textarea");
-    el.value = text;
-    document.body.appendChild(el);
-    const selected = selection.rangeCount > 0 ? selection.getRangeAt(0) : false;
-    el.select();
-    // eslint-disable-next-line @typescript-eslint/no-deprecated
-    document.execCommand("copy"); // required by puppeteer tests
-    el.remove();
-    if (selected) {
-        selection.removeAllRanges();
-        selection.addRange(selected);
-    }
-}
-
-function copyHeaders(headers: string[]) {
-    copyToClipboard(headers.join("\t"));
+async function copyHeaders(headers: string[]): Promise<void> {
+    await navigator.clipboard.writeText(headers.join("\t"));
 }
