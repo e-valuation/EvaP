3a6312b58d8f7f15f8a0a2cd23538bde86427c8f
Niklas Mohrin <dev@niklasmohrin.de>
2025-03-11 21:31:19 +0100

    Add `evap-dev-with-lsp` shell (#2407)

--- flake.nix
+++ flake.nix
@@ -46,6 +46,7 @@
             workspaceRoot = ./.;
           };
           evap-dev = evap.override (prev: { dependency-groups = (prev.dependency-groups or [ ]) ++ [ "dev" ]; });
+          evap-dev-with-lsp = evap-dev.override (prev: { dependency-groups = (prev.dependency-groups or [ ]) ++ [ "lsp" ]; });
           evap-frontend-dev = evap-dev.overrideAttrs (prev: { nativeBuildInputs = (prev.nativeBuildInputs or [ ]) ++ (with pkgs; [ firefox geckodriver ]); });
           default = evap-dev;
 
