738fcbc8767c0b8d5f1e82f5edb0836a4894bdcd
Josef Valentin <josef.valentin2004@gmail.com>
2025-03-23 21:30:13 +0100

    removed dynamically getting field_name
    since the implementation would find ANY M2M field that contains UserProfiles.

--- evap/staff/forms.py
+++ evap/staff/forms.py
@@ -1011,42 +1011,23 @@ class UserForm(forms.ModelForm):
 
     def save(self, *args, **kw):  # noqa: PLR0912
         super().save(*args, **kw)
-        new_evaluation_list = list(
-            self.instance.evaluations_participating_in.exclude(course__semester=Semester.active_semester())
-        ) + list(self.cleaned_data.get("evaluations_participating_in"))
-
-        # Since a user cannot be in in the participants list of an evaluation multiple times, sets can be used.
+        evaluations_old_semester = self.instance.evaluations_participating_in.exclude(
+            course__semester=Semester.active_semester()
+        )
+        new_evaluations = set(evaluations_old_semester) | set(self.cleaned_data.get("evaluations_participating_in"))
         old_evaluations = set(self.instance.evaluations_participating_in.all())
-        new_evaluations = set(
-            self.instance.evaluations_participating_in.exclude(course__semester=Semester.active_semester())
-        ) | set(self.cleaned_data.get("evaluations_participating_in"))
+
+        self.instance.evaluations_participating_in.set(list(new_evaluations))
+
         added_evaluations = new_evaluations - old_evaluations
         removed_evaluations = old_evaluations - new_evaluations
-
-        self.instance.evaluations_participating_in.set(new_evaluation_list)
-
-        field_name = None
-        # This only works if there is only one m2m field which contains a Userprofile, otherwise a different field could be identified.
         if added_evaluations or removed_evaluations:
-            example_evaluation = next(iter(added_evaluations | removed_evaluations), None)
-            if example_evaluation and isinstance(example_evaluation, LoggedModel):
-                for field in type(example_evaluation)._meta.many_to_many:
-                    related_model = getattr(field.remote_field, "model", None)
-                    if isinstance(self.instance, related_model):
-                        # if related_model == type(self.instance):
-                        field_name = field.name
-                        break
-
-        if field_name is not None:
             for evaluation in added_evaluations:
-                if isinstance(evaluation, LoggedModel):
-                    if field_name not in evaluation.unlogged_fields:
-                        evaluation.log_m2m_change(field_name, FieldActionType.M2M_ADD, [self.instance.pk])
-
+                if isinstance(evaluation, LoggedModel) and "participants" not in evaluation.unlogged_fields:
+                    evaluation.log_m2m_change("participants", FieldActionType.M2M_ADD, [self.instance.pk])
             for evaluation in removed_evaluations:
-                if isinstance(evaluation, LoggedModel):
-                    if field_name not in evaluation.unlogged_fields:
-                        evaluation.log_m2m_change(field_name, FieldActionType.M2M_REMOVE, [self.instance.pk])
+                if isinstance(evaluation, LoggedModel) and "participants" not in evaluation.unlogged_fields:
+                    evaluation.log_m2m_change("participants", FieldActionType.M2M_REMOVE, [self.instance.pk])
 
         manager_group = Group.objects.get(name="Manager")
         grade_publisher_group = Group.objects.get(name="Grade publisher")
