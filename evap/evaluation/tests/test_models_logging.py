e87ad3b51e39b5972abfe3f3b7aae721638fffe7
Josef Valentin <josef.valentin2004@gmail.com>
2025-04-15 11:54:27 +0200

    fixup! test reverse m2m logs

--- evap/evaluation/tests/test_models_logging.py
+++ evap/evaluation/tests/test_models_logging.py
@@ -1,10 +1,11 @@
 from datetime import date, datetime, timedelta
+from unittest.mock import patch
 
 from django.utils.formats import localize
 from model_bakery import baker
 
 from evap.evaluation.models import Contribution, Course, Evaluation, Questionnaire, UserProfile
-from evap.evaluation.models_logging import FieldAction, InstanceActionType
+from evap.evaluation.models_logging import FieldAction, InstanceActionType, _m2m_changed
 from evap.evaluation.tests.tools import TestCase
 
 
@@ -148,15 +149,43 @@ class TestLoggedModel(TestCase):
             {"participants": {"add": [participant2.id], "remove": [participant1.id]}},
         )
 
-    def test_logging_respects_unlogged_fields(self):
+    def test_logging_m2m_reverse_changes(self):
         participant = baker.make(UserProfile)
 
-        # Temporarily modify unlogged_fields to include 'participants'
+        participant.evaluations_participating_in.add(self.evaluation)
+        self.assertEqual(
+            self.evaluation.related_logentries().order_by("id").last().data,
+            {"participants": {"add": [participant.id]}},
+        )
+
+        participant.evaluations_participating_in.remove(self.evaluation)
+        self.assertEqual(
+            self.evaluation.related_logentries().order_by("id").last().data,
+            {"participants": {"remove": [participant.id]}},
+        )
+
+        participant.evaluations_participating_in.add(self.evaluation)
+        participant.evaluations_participating_in.clear()
+        self.assertEqual(
+            self.evaluation.related_logentries().order_by("id").last().data,
+            {"participants": {"remove": [participant.id]}},
+        )
+
+        result_no_field_name = _m2m_changed(None, None, None, True, Evaluation, None)
+        self.assertEqual(result_no_field_name, None)
+
+    def test_logging_respects_unlogged_fields(self):
+        participant = baker.make(UserProfile)
         original_unlogged_fields = self.evaluation.unlogged_fields
-        self.evaluation.__class__.unlogged_fields = property(lambda self: original_unlogged_fields + ["participants"])
 
-        self.evaluation.participants.add(participant)
-        self.assertFalse(any("participants" in entry.data for entry in self.evaluation.related_logentries()))
+        # Temporarily modify unlogged_fields to include 'participants'
+        with patch.object(
+            self.evaluation.__class__,
+            "unlogged_fields",
+            new=property(lambda self: original_unlogged_fields + ["participants"]),
+        ):
+            self.evaluation.participants.add(participant)
+            self.assertFalse(any("participants" in entry.data for entry in self.evaluation.related_logentries()))
 
-        # Restore original unlogged_fields or other test might have unexpected behavior
-        self.evaluation.__class__.unlogged_fields = property(lambda self: original_unlogged_fields)
+            participant.evaluations_participating_in.add(self.evaluation)
+            self.assertFalse(any("participants" in entry.data for entry in self.evaluation.related_logentries()))
