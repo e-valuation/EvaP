f07f4b16482d18920ccaace70605da72bffa581c
Niklas Mohrin <dev@niklasmohrin.de>
2025-05-26 18:32:21 +0200

    Move check-dist.py to tools/

--- flake.nix
+++ flake.nix
@@ -124,7 +124,7 @@
             let
               py = self.packages.${system}.python3.withPackages (ps: [ ps.pathspec ]);
             in
-            pkgs.writeShellScriptBin "check-dist" "${py}/bin/python ${./nix/check-dist.py} $@";
+            pkgs.writeShellScriptBin "check-dist" "${py}/bin/python ${./tools/check-dist.py} $@";
 
           clean-setup = pkgs.writeShellApplication {
             name = "clean-setup";
