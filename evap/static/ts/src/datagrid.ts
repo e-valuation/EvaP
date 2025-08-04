b5b3f3d979d1372ac95564e7ec560444a03e07fa
Felix Koch <felixkoch.it@gmail.com>
2025-08-04 21:50:55 +0200

    fix reset & filter

--- evap/static/ts/src/datagrid.ts
+++ evap/static/ts/src/datagrid.ts
@@ -271,17 +271,22 @@ export class TableGrid extends DataGrid {
 
 interface EvaluationGridParameters extends DataGridParameters {
     filterButtons: HTMLButtonElement[];
+    resetFilterButton: HTMLButtonElement;
 }
 
 export class EvaluationGrid extends DataGrid {
+    // TODO: rename class
+    // TODO: maybe merge with some others?
     protected findSearchableCells(row: HTMLElement): HTMLElement[] {
         return [...row.children] as HTMLElement[]; // TODO: skip buttons?
     }
     private filterButtons: HTMLButtonElement[];
+    private resetFilterButton: HTMLButtonElement;
 
-    constructor({ filterButtons, ...options }: EvaluationGridParameters) {
+    constructor({ filterButtons, resetFilterButton, ...options }: EvaluationGridParameters) {
         super(options);
         this.filterButtons = filterButtons;
+        this.resetFilterButton = resetFilterButton;
     }
 
     public bindEvents() {
@@ -304,6 +309,15 @@ export class EvaluationGrid extends DataGrid {
                 this.filterRows();
                 this.renderToDOM();
             });
+        });
+
+        this.resetFilterButton.addEventListener("click", () => {
+            this.state.search = "";
+            this.state.equalityFilter.clear();
+            this.state.rangeFilter.clear();
+            this.filterRows();
+            this.renderToDOM();
+            this.reflectFilterStateOnInputs();
         });
     }
 
