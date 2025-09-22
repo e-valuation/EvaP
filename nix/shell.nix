f77ac7d3fce6e4f955ed84ee77329e70fed76a46
Niklas Mohrin <dev@niklasmohrin.de>
2025-09-22 20:05:15 +0200

    p

--- nix/shell.nix
+++ nix/shell.nix
@@ -59,7 +59,6 @@ pkgs.mkShell {
 
   env = {
     DJANGO_SETTINGS_MODULE = "localsettings"; # this file is set up by services-full
-    PUPPETEER_SKIP_DOWNLOAD = 1;
     UV_NO_SYNC = "1";
     UV_PYTHON = python3.interpreter;
     UV_PYTHON_DOWNLOADS = "never";
