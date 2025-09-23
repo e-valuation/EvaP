358fe2ae4abd7f57391cd506382f5214474e7b78
Niklas Mohrin <dev@niklasmohrin.de>
2025-09-23 17:55:41 +0200

    include debug_toolbar settings in localsettings template

--- evap/development/localsettings.template.py
+++ evap/development/localsettings.template.py
@@ -1,6 +1,6 @@
 # noqa: N999
 
-from evap.settings import default, dev, local_services, open_id, test  # noqa: TID251
+from evap.settings import debug_toolbar, default, dev, local_services, open_id, test  # noqa: TID251
 from evap.settings_resolver import resolve_settings
 
 
@@ -9,4 +9,4 @@ class LocalSettings:
     SECRET_KEY = "$SECRET_KEY"  # nosec
 
 
-globals().update(resolve_settings([default, local_services, open_id, dev, test, LocalSettings]))
+globals().update(resolve_settings([default, local_services, open_id, dev, test, debug_toolbar, LocalSettings]))
