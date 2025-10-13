602c3a383b91ba2b9e778116ef4ed8501e6f1a38
Felix Koch <felixkoch.it@gmail.com>
2025-10-13 20:50:04 +0200

    Visual Regression Test

--- evap/staff/tests/test_live.py
+++ evap/staff/tests/test_live.py
@@ -1,7 +1,10 @@
+import time
 from datetime import date, datetime
 
 from model_bakery import baker
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.expected_conditions import (
     element_to_be_clickable,
     invisibility_of_element_located,
@@ -20,6 +23,7 @@ from evap.evaluation.models import (
 )
 from evap.evaluation.tests.tools import LiveServerTest
 
+from visual_regression_tracker import VisualRegressionTracker, Config, TestRun, IgnoreArea
 
 class EvaluationEditLiveTest(LiveServerTest):
     def test_submit_changes_form_data(self):
@@ -111,3 +115,75 @@ class EvaluationEditLiveTest(LiveServerTest):
         search_input.send_keys("exam")
 
         self.wait.until(invisibility_of_element_located((By.XPATH, "//td//a[contains(text(),'course name')]")))
+
+class StaffSemesterViewRegressionTest(LiveServerTest):
+
+    def test_regression(self):
+        vrt = VisualRegressionTracker()
+
+        baker.seed(31902)
+
+        responsible = baker.make(UserProfile)
+        evaluation = baker.make(
+            Evaluation,
+            course=baker.make(Course, programs=[baker.make(Program)], responsibles=[responsible]),
+            vote_start_datetime=datetime(2099, 1, 1, 0, 0),
+            vote_end_date=date(2099, 12, 31),
+            main_language="en",
+        )
+        evaluation2 = baker.make(
+            Evaluation,
+            course=baker.make(Course, semester=evaluation.course.semester, programs=[baker.make(Program)], responsibles=[responsible]),
+            vote_start_datetime=datetime(2099, 1, 1, 0, 0),
+            vote_end_date=date(2099, 12, 31),
+            main_language="en",
+        )
+        evaluation3 = baker.make(
+            Evaluation,
+            course=baker.make(Course, semester=evaluation.course.semester, programs=[baker.make(Program)], responsibles=[responsible]),
+            vote_start_datetime=datetime(2099, 1, 1, 0, 0),
+            vote_end_date=date(2099, 12, 31),
+            main_language="de",
+        )
+
+        general_questionnaire = baker.make(Questionnaire, questions=[baker.make(Question)])
+        evaluation.general_contribution.questionnaires.set([general_questionnaire])
+
+        contribution1 = baker.make(
+            Contribution,
+            evaluation=evaluation,
+            contributor=responsible,
+            order=0,
+            role=Contribution.Role.CONTRIBUTOR,
+            textanswer_visibility=Contribution.TextAnswerVisibility.OWN_TEXTANSWERS,
+        )
+        baker.make(
+            Contribution,
+            evaluation=evaluation,
+            contributor=baker.make(UserProfile),
+            order=1,
+            role=Contribution.Role.EDITOR,
+        )
+
+        with vrt:
+            with self.enter_staff_mode():
+                self.selenium.get(self.live_server_url + reverse("staff:semester_view", args=[evaluation.course.semester_id]))
+
+
+                _ = self.wait.until(
+                    expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '#evaluation-filter-buttons .badge'))
+                )
+
+                vrt.track(TestRun(
+                    name='staff:index',
+                    imageBase64=self.selenium.get_screenshot_as_base64(),
+                    diffTollerancePercent=0,
+                    os='Linux',
+                    browser='Firefox',
+                    viewport=self.viewport,
+                    device='PC',
+                    ignoreAreas=[IgnoreArea(y=3968, height=42, x=0, width=1920)]
+                ))
+
+
+
