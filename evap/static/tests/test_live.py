c17def02c9851edf964b6663032b3bacd9355dca
gitjannes <jannesk@gmx.net>
2025-10-27 21:13:39 +0100

    added width approximation

--- evap/static/tests/test_live.py
+++ evap/static/tests/test_live.py
@@ -6,6 +6,7 @@ from evap.evaluation.tests.tools import LiveServerTest
 
 class NoteBookPlacementLiveTest(LiveServerTest):
     def test_notebook_placement_big_width(self):
+        self.selenium.get(self.live_server_url)
         self.selenium.set_window_size(1400, 900)
         self.selenium.find_element(By.ID, "notebookButton").click()
 
@@ -20,6 +21,7 @@ class NoteBookPlacementLiveTest(LiveServerTest):
         self.assertEqual(margin_top, "0px")
 
     def test_notebook_placement_large_aspect_ratio(self):
+        self.selenium.get(self.live_server_url)
         self.selenium.set_window_size(1399, 932)  # aspect ratio just above 3:2 and width below 1400px
         self.selenium.find_element(By.ID, "notebookButton").click()
         notebook = self.wait.until(visibility_of_element_located((By.ID, "notebook")))
@@ -33,12 +35,13 @@ class NoteBookPlacementLiveTest(LiveServerTest):
         self.assertEqual(margin_top, "0px")
 
     def test_notebook_placement_small_aspect_ratio(self):
+        self.selenium.get(self.live_server_url)
         self.selenium.set_window_size(1399, 934)  # aspect ratio just below 3:2 and width below 1400px
         self.selenium.find_element(By.ID, "notebookButton").click()
         notebook = self.wait.until(visibility_of_element_located((By.ID, "notebook")))
         width = notebook.value_of_css_property("width")
 
-        self.assertEqual(width, "100vw")
+        self.assertEqual(width, "80vw")
 
         evap_content = self.selenium.find_element(By.ID, "evapContent")
         margin_top = evap_content.value_of_css_property("margin-top")
