ec14a0cdd28c29f9d2087789cefe4289b957ed9d
Pipifax <37810842+Belissimo-T@users.noreply.github.com>
2025-11-10 22:01:52 +0100

    fix #2550

--- evap/evaluation/management/commands/lint.py
+++ evap/evaluation/management/commands/lint.py
@@ -4,14 +4,28 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    args = ""
     help = "Runs the code linter"
     requires_migrations_checks = False
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        parser.add_argument("linter", nargs="?", choices=["ruff", "pylint", "eslint"], help="Specify a linter to run.")
+
+    def run_ruff(self):
         self.stdout.write("Executing ruff check .")
         subprocess.run(["ruff", "check", "."], check=False)  # nosec
+
+    def run_pylint(self):
         self.stdout.write("Executing pylint evap")
         subprocess.run(["pylint", "evap", "tools"], check=False)  # nosec
+
+    def run_eslint(self):
         self.stdout.write("Executing npx eslint --quiet")
         subprocess.run(["npx", "eslint", "--quiet"], cwd="evap/static/ts", check=False)  # nosec
+
+    def handle(self, *args, **options):
+        if options["linter"] in ("ruff", None):
+            self.run_ruff()
+        if options["linter"] in ("pylint", None):
+            self.run_pylint()
+        if options["linter"] in ("eslint", None):
+            self.run_eslint()
