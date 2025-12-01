e7294133613654e32ed3dc67bfb34a9b95aadfdc
suttrop <239040530+suttrop@users.noreply.github.com>
2025-12-01 21:27:47 +0100

    use ruff format instead of black

--- evap/evaluation/management/commands/format.py
+++ evap/evaluation/management/commands/format.py
@@ -10,25 +10,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "formatter", nargs="?", choices=["black", "isort", "prettier", "python"], help="Specify a formatter to run."
+            "formatter", nargs="?", choices=["ruff", "prettier", "python"], help="Specify a formatter to run."
         )
 
-    def run_black(self):
-        self.stdout.write("Executing black .")
-        subprocess.run(["black", "."], check=False)  # nosec
-
-    def run_isort(self):
-        self.stdout.write("Executing isort .")
-        subprocess.run(["isort", "."], check=False)  # nosec
+    def run_ruff(self):
+        self.stdout.write("Executing ruff format .")
+        subprocess.run(["ruff", "format", "."], check=False)  # nosec
 
     def run_prettier(self):
         self.stdout.write("Executing npx prettier")
         subprocess.run(["npx", "prettier", "--write", "evap/static/ts/**/*.ts"], check=False)  # nosec
 
     def handle(self, *args, **options):
-        if options["formatter"] in ("black", "python", None):
-            self.run_black()
-        if options["formatter"] in ("isort", "python", None):
-            self.run_isort()
+        if options["formatter"] in ("ruff", "python", None):
+            self.run_ruff()
         if options["formatter"] in ("prettier", None):
             self.run_prettier()
