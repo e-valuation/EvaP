302c5258244188fdcdc44cb3d14c1ecb57b809cf
Felix Koch <felixkoch.it@gmail.com>
2025-12-01 21:51:29 +0100

    set branchname via context

--- evap/evaluation/management/commands/vrt_test.py
+++ evap/evaluation/management/commands/vrt_test.py
@@ -17,21 +17,25 @@ class Command(BaseCommand):
         assert os.environ.get("VRT_APIKEY"), "env var VRT_APIKEY must be set"
         assert os.environ.get("VRT_PROJECT"), "env var VRT_PROJECT must be set"
 
-        commit_hash = run(["git", "rev-parse", "--short", "HEAD"], capture_output=True, check=False)
-        if commit_hash.returncode != 0:
-            self.stderr.write(self.style.ERROR("Could not get commit hash: " + str(commit_hash.stderr)))
-            sys.exit(1)
-        commit_hash = commit_hash.stdout.decode().strip()
-        self.stdout.write(self.style.NOTICE(f"using VRT_CIBUILDID = '{commit_hash}'"))
-        os.environ["VRT_CIBUILDID"] = commit_hash
+        commit_hash = os.environ.get("VRT_CIBUILDID")
+        if not commit_hash:
+            commit_hash = run(["git", "rev-parse", "--short", "HEAD"], capture_output=True, check=False)
+            if commit_hash.returncode != 0:
+                self.stderr.write(self.style.ERROR("Could not get commit hash: " + str(commit_hash.stderr)))
+                sys.exit(1)
+            commit_hash = commit_hash.stdout.decode().strip()
+            self.stdout.write(self.style.NOTICE(f"using VRT_CIBUILDID = '{commit_hash}'"))
+            os.environ["VRT_CIBUILDID"] = commit_hash
 
-        branch_name = run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, check=False)
-        if branch_name.returncode != 0:
-            self.stderr.write(self.style.ERROR("Could not get branch name: " + branch_name.stderr))
-            sys.exit(1)
-        branch_name = branch_name.stdout.decode().strip()
-        self.stdout.write(self.style.NOTICE(f"using VRT_BRANCHNAME = '{branch_name}'"))
-        os.environ["VRT_BRANCHNAME"] = branch_name
+        branch_name = os.environ.get("VRT_BRANCHNAME")
+        if not branch_name:
+            branch_name = run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, check=False)
+            if branch_name.returncode != 0:
+                self.stderr.write(self.style.ERROR("Could not get branch name: " + branch_name.stderr))
+                sys.exit(1)
+            branch_name = branch_name.stdout.decode().strip()
+            self.stdout.write(self.style.NOTICE(f"using VRT_BRANCHNAME = '{branch_name}'"))
+            os.environ["VRT_BRANCHNAME"] = branch_name
 
         call_command("ts", "compile")
         call_command("scss")
