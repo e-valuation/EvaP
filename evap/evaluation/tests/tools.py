32110d4a27c4eb3339e28c2229b0770c35acea25
Pipifax (mit der Lupe 🔎) <37810842+Belissimo-T@users.noreply.github.com>
2025-12-01 21:58:57 +0100

    Add `--headed` flag for live tests (#2572)
    fix #2567

--- evap/evaluation/tests/tools.py
+++ evap/evaluation/tests/tools.py
@@ -43,10 +43,28 @@ from evap.evaluation.models import (
 class EvapTestRunner(DiscoverRunner):
     """Skips selenium tests by default, if no other tags are specified."""
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, *args: Any, headed=False, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
+
+        self.__headed = headed
+
         if not self.tags and not self.exclude_tags:
             self.exclude_tags = {"selenium"}
+
+    @classmethod
+    def add_arguments(cls, parser):
+        super().add_arguments(parser)
+
+        parser.add_argument(
+            "--headed",
+            help="Run the tests in non-headless mode, which makes the browser window visible. Useful for debugging.",
+            action="store_true",
+        )
+
+    def setup_test_environment(self, **kwargs):
+        super().setup_test_environment(**kwargs)
+
+        LiveServerTest.headless = not self.__headed
 
 
 class ResetLanguageOnTearDownMixin:
