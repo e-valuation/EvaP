8802c8f73650e2f1cdc06344f1eeb5b7720d336e
Johannes Linke <johannes.linke@posteo.de>
2025-12-15 15:33:13 +0100

    Update to django 6.0

--- evap/settings.py
+++ evap/settings.py
@@ -248,6 +248,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "django_extensions",
     "evap.evaluation",
     "evap.staff",
