b45f8e0fe0f90f23934df780e6e86be0db6f094c
Johannes Wolf <janno42@posteo.de>
2025-12-16 11:26:42 +0100

    show exam type in contributor evaluation form

--- evap/contributor/forms.py
+++ evap/contributor/forms.py
@@ -31,6 +31,7 @@ class EvaluationForm(forms.ModelForm):
             "name_de_field",
             "name_en_field",
             "main_language",
+            "exam_type",
             "vote_start_datetime",
             "vote_end_date",
             "participants",
@@ -47,6 +48,10 @@ class EvaluationForm(forms.ModelForm):
 
         self.fields["name_de_field"].initial = self.instance.full_name_de
         self.fields["name_en_field"].initial = self.instance.full_name_en
+
+        self.fields["exam_type"].disabled = True
+        if self.instance.exam_type is None:
+            self.fields["exam_type"].widget = self.fields["exam_type"].hidden_widget()
 
         self.fields["general_questionnaires"].queryset = (
             Questionnaire.objects.general_questionnaires()
