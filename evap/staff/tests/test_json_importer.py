0a675694b2191fc082392cc7787bcb5cc37e3957
Lukas <100872697+LukasMansour@users.noreply.github.com>
2026-01-12 18:46:54 +0100

    Fix logging empty m2m changes, #2576

--- evap/staff/tests/test_json_importer.py
+++ evap/staff/tests/test_json_importer.py
@@ -711,6 +711,8 @@ class TestImportEvents(TestCase):
         evaluation.course.name_en = "Change"
         evaluation.course.save()
 
+        # There is still an update occuring here, but empty m2m changes are no longer an issue.
+        # with assert_no_database_modifications():
         importer = self._import()
 
         evaluation = Evaluation.objects.get(pk=evaluation.pk)
