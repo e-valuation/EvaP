d3bd5c12e23d9cc87368363c424f75146c05dcf1
Johannes Wolf <dev@janno42.de>
2026-02-17 16:43:31 +0100

    disable manual participant changes for all linked evaluations for all users

--- evap/contributor/forms.py
+++ evap/contributor/forms.py
@@ -86,9 +86,9 @@ class EvaluationForm(forms.ModelForm):
             for field in self._meta.fields:
                 self.fields[field].disabled = True
 
-        if self.instance.exam_type is not None and self.instance.evaluation_links.exists():
+        if self.instance.evaluation_links.filter(is_active=True).exists():
             self.fields["participants"].disabled = True
-            self.cms_disclaimer = _("Participants are regularly updated with exam registrations from the CMS.")
+            self.cms_disclaimer = _("Participants are regularly updated with registrations from the CMS.")
 
     def clean(self):
         super().clean()
--- evap/staff/forms.py
+++ evap/staff/forms.py
@@ -479,6 +479,10 @@ class EvaluationForm(forms.ModelForm):
         if self.instance.pk:
             self.instance.old_course = self.instance.course
 
+        if self.instance.pk and self.instance.evaluation_links.filter(is_active=True).exists():
+            self.fields["participants"].disabled = True
+            self.cms_disclaimer = _("Participants are regularly updated with registrations from the CMS.")
+
     def validate_unique(self):
         super().validate_unique()
         # name_xy and course are unique together. This will be treated as a non-field-error since two
