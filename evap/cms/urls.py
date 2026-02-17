d86f681b8bc0a6ad5972ba96b4d2f808d83e8eb8
Johannes Wolf <dev@janno42.de>
2026-02-17 13:25:29 +0100

    add link badges and allow deactivating CourseLinks and EvaluationLinks

--- evap/cms/urls.py
+++ evap/cms/urls.py
@@ -6,4 +6,7 @@ app_name = "cms"
 
 urlpatterns = [
     path("ignored_evaluation/delete", views.ignored_evaluation_delete, name="ignored_evaluation_delete"),
+    path("cms_course_link_update_activation", views.cms_course_link_update_activation, name="cms_course_link_update_activation"),
+    path("cms_evaluation_link_update_activation", views.cms_evaluation_link_update_activation, name="cms_evaluation_link_update_activation"),
+    path("evaluation_merge_selection/<int:main_evaluation_id>", views.evaluation_merge_selection, name="evaluation_merge_selection"),
 ]
