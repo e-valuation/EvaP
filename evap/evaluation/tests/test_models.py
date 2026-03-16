031b579fd1d8cb9a53b307b0cbf4899fce69e05e
Niklas Mohrin <dev@niklasmohrin.de>
2026-03-16 21:30:01 +0100

    Run QuestionAssignment's delete garbage collection in signal (#2675)
    The model's `.delete` method is not called when an object is deleted
    from a queryset or in a cascading delete. Instead, the `post_delete`
    signal can be used. I also made sure to wrap the handler in a
    transaction to rule out this kind of error.
    
    See
    https://docs.djangoproject.com/en/6.0/topics/db/models/#overriding-model-methods
    and https://github.com/e-valuation/EvaP/pull/2513#discussion_r2941714540
    
    - Run QuestionAssignment's delete garbage collection in signal
    - Merge `TestQuestionnaire` and `QuestionnaireTests`, and downgrade to
    TestCase

--- evap/evaluation/tests/test_models.py
+++ evap/evaluation/tests/test_models.py
@@ -17,6 +17,7 @@ from evap.evaluation.models import (
     Evaluation,
     NotArchivableError,
     Question,
+    QuestionAssignment,
     Questionnaire,
     QuestionType,
     Semester,
@@ -58,13 +59,37 @@ class TestSemester(WebTest):
         self.assertTrue(semester.can_be_deleted_by_manager)
 
 
-class TestQuestionnaire(WebTest):
+class TestQuestionnaire(TestCase):
     def test_can_be_deleted_by_manager(self):
         questionnaire = baker.make(Questionnaire)
         self.assertTrue(questionnaire.can_be_deleted_by_manager)
 
         baker.make(Contribution, questionnaires=[questionnaire])
         self.assertFalse(questionnaire.can_be_deleted_by_manager)
+
+    def test_locked_contributor_questionnaire(self):
+        questionnaire = baker.prepare(Questionnaire, is_locked=True, type=Questionnaire.Type.CONTRIBUTOR)
+        self.assertRaises(ValidationError, questionnaire.clean)
+
+
+class TestQuestionAssignment(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.assignment = baker.make(QuestionAssignment)
+        cls.question = cls.assignment.question
+        cls.questionnaire = cls.assignment.questionnaire
+
+    def test_assignment_delete_gc(self):
+        self.assignment.delete()
+        self.assertRaises(Question.DoesNotExist, self.question.refresh_from_db)
+
+    def test_assignment_queryset_delete_gc(self):
+        QuestionAssignment.objects.filter(pk=self.assignment.pk).delete()
+        self.assertRaises(Question.DoesNotExist, self.question.refresh_from_db)
+
+    def test_questionnaire_cascading_delete_gc(self):
+        self.questionnaire.delete()
+        self.assertRaises(Question.DoesNotExist, self.question.refresh_from_db)
 
 
 @override_settings(EVALUATION_END_OFFSET_HOURS=0)
@@ -1148,9 +1173,3 @@ class TestEmailRecipientList(TestCase):
             evaluation, [EmailTemplate.Recipients.CONTRIBUTORS], filter_users_in_cc=True
         )
         self.assertCountEqual(recipient_list, [contributor2, contributor3])
-
-
-class QuestionnaireTests(TestCase):
-    def test_locked_contributor_questionnaire(self):
-        questionnaire = baker.prepare(Questionnaire, is_locked=True, type=Questionnaire.Type.CONTRIBUTOR)
-        self.assertRaises(ValidationError, questionnaire.clean)
