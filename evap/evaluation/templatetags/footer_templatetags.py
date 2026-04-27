2f49aa1f185d616bf0784a3d79aa8c44415d347c
suttrop <239040530+suttrop@users.noreply.github.com>
2026-04-27 21:26:18 +0200

    add template tag

--- evap/evaluation/templatetags/footer_templatetags.py
+++ evap/evaluation/templatetags/footer_templatetags.py
@@ -7,3 +7,8 @@ register = Library()
 @register.simple_tag
 def release_version():
     return settings.RELEASE_VERSION
+
+
+@register.simple_tag
+def legal_link():
+    return settings.LEGAL_NOTICE_LINK
