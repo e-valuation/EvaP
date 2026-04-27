bf289e02ab8e30800e2eab26fad39e6c815b846e
suttrop <239040530+suttrop@users.noreply.github.com>
2026-04-27 21:26:58 +0200

    introduce LEGAL_NOTICE_LINK

--- evap/settings.py
+++ evap/settings.py
@@ -189,7 +189,7 @@ STORAGES = {
 
 CONTACT_EMAIL = "webmaster@localhost"
 ALLOW_ANONYMOUS_FEEDBACK_MESSAGES = True
-LEGAL_NOTICE_TEXT = "Objection! (this is a default setting that the administrators should change, please contact them)"
+LEGAL_NOTICE_LINK = "http://example.com/legalnotice"
 
 # Config for mail system
 DEFAULT_FROM_EMAIL = "webmaster@localhost"
