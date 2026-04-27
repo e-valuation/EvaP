042cb29b9159cbd8ff1d3b02d9ebea41fc38858d
ZoqkMaze <155395129+ZoqkMaze@users.noreply.github.com>
2026-04-27 22:27:58 +0200

    add global listener

--- evap/static/ts/src/base-template.ts
+++ evap/static/ts/src/base-template.ts
@@ -11,3 +11,13 @@ document.addEventListener("shown.bs.modal", e => {
         autofocusEl.focus();
     }
 });
+
+document.addEventListener("click", e => {
+    if (!e.target) {return;}
+    const modalEl = e.target as HTMLElement;
+    if (!modalEl.hasAttribute("data-copy-on-click")) {return;}
+    const copy_string = modalEl.getAttribute("data-copy-on-click")?.toString();
+    if (copy_string) {
+        navigator.clipboard.writeText(copy_string);
+    }
+});
