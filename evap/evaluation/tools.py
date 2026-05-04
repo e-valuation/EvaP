8e1bcccee865b2bde1966e20c83690feb837ddba
ZoqkMaze <155395129+ZoqkMaze@users.noreply.github.com>
2026-05-04 21:16:23 +0200

    T

--- evap/evaluation/tools.py
+++ evap/evaluation/tools.py
@@ -176,9 +176,9 @@ def get_bool_parameter_from_url_or_session(request: HttpRequest, parameter: str,
     )
 
 
-def get_string_parameter_from_url_or_session[TDefault: (typing.Any, None)](
-    request: HttpRequest, parameter: str, default: TDefault = None
-) -> str | TDefault:
+def get_string_parameter_from_url_or_session[T: (typing.Any, None)](
+    request: HttpRequest, parameter: str, default: T = None
+) -> str | T:
     result = request.GET.get(parameter, None)
     if result is None:
         result = request.session.get(parameter, default)
