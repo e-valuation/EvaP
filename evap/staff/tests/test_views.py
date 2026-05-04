504bd997d9e1095b2a8e98d74278f997f6d1fc48
Jakob <jakorab3@gmail.com>
2026-05-04 19:31:36 +0200

    test moved to new function

--- evap/staff/tests/test_views.py
+++ evap/staff/tests/test_views.py
@@ -2465,16 +2465,15 @@ class TestEvaluationPreviewView(WebTestStaffModeWith200Check):
         cls.test_users = [cls.manager]
         cls.url = reverse("staff:evaluation_preview", args=[cls.evaluation.pk])
 
-        cls.evaluation_un = baker.make(Evaluation, main_language=Evaluation.UNDECIDED_MAIN_LANGUAGE)
-        cls.url2 = reverse("staff:evaluation_preview", args=[cls.evaluation_un.pk])
-        cls.evaluation_un.general_contribution.questionnaires.set([baker.make(Questionnaire)])
-
     def test_without_questionnaires_assigned(self):
         # regression test for #1747
         self.evaluation.general_contribution.questionnaires.set([])
         self.app.get(self.url, user=self.manager, status=200)
 
     def test_lang_undecided(self):
+        self.evaluation_un = baker.make(Evaluation, main_language=Evaluation.UNDECIDED_MAIN_LANGUAGE)
+        self.url2 = reverse("staff:evaluation_preview", args=[self.evaluation_un.pk])
+        self.evaluation_un.general_contribution.questionnaires.set([baker.make(Questionnaire)])
         self.app.get(self.url2, user=self.manager, status=200)
 
 
